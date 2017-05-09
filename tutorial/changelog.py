f1d95ab30441428669b05784625d540c355cf46a
chriddyp <chris@plot.ly>
2017-05-09 13:05:30 -0700

    add changelog entry for dash-renderer

--- tutorial/changelog.py
+++ tutorial/changelog.py
@@ -5,4 +5,21 @@ layout = dcc.Markdown('''
 
 Updates and breaking changes from the dash community will be listed in
 this changelog.
+
+## dash-renderer [0.6.1] - 2017-05-09
+
+### Fixed
+
+`dash-renderer` version 0.6.1 fixes an intermittent bug
+that occurs when certain requests take longer than
+others to complete.
+
+The bug manifests itself in a message that says
+**Error loading layout** or sometimes
+**Error loading dependencies**.
+
+Upgrade with:
+```
+pip install dash-renderer==0.6.1
+```
 ''')
