de43ea19069ceed1dd0dbe734b2fa0bfa6dc86ca
chriddyp <chris@plot.ly>
2017-06-12 22:27:37 -0400

    update introduction

--- tutorial/introduction.py
+++ tutorial/introduction.py
@@ -2,39 +2,43 @@ import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Input, Output, Event, State
 import datetime
-from pyorbital.orbital import Orbital
-import os
 
 from server import app
 
 layout = html.Div([
     dcc.Markdown('''
-        ## Dash User Guide
+        ## Introduction to Dash
 
-        Dash is a productive python framework for building web applications.
+        Dash is a productive Python framework for building web applications.
 
-        Written on top of Plotly.js and React.js,
+        Written on top of Flask, Plotly.js, and React.js,
         Dash is ideal for building data visualization apps
-        with highly custom user interfaces.
+        with highly custom user interfaces in pure Python.
+        It's particuarly suited for anyone who works with data in Python.
 
-        This is an exclusive developer preview of Dash.
-        Dash is currently unreleased and unannounced.
-        Please do not share Dash without Plotly's constent.
+        Through a couple of simple patterns, Dash abstracts away all of the
+        technologies and protocols that are required to build an
+        interactive web-based application.
+        Dash is simple enough that you can bind a user interface
+        around your Python code in an afternoon.
 
-        The core functionality of Dash will be open sourced.
-        For enterprises, Plotly offers a platform for
-        deploying, orchestrating, and permissioning dash apps behind
-        your firewall. If you're interested,
-        please get in touch at
-        [https://plot.ly/products/consulting-and-oem/](https://plot.ly/products/consulting-and-oem/)
-        to register for early access.
+        Dash apps are rendered in the web browser. You can deploy your apps
+        to servers and then share them through URLs.
+        Since Dash apps are viewed in the web browser, Dash is inherently
+        cross-platform and mobile ready.
+
+        There is a lot behind the framework. To learn more about how it is built
+        and what motivated dash, watch our talk from
+        [Plotcon](https://plotcon.plot.ly) below
+        or read our [announcement letter](https://medium.com/@chriddyp/introducing-dash-a-web-app-framework-for-python-data-science-942e37f38ce1).
+
+        Dash is an open source library, released under the permissive MIT license.
+        [Plotly](https://plot.ly) develops Dash and offers a platform for
+        deploying, orchestrating, and permissioning dash apps in an enterprise
+        environment. If you're interested, please get in touch at
+        [https://plot.ly/products/consulting-and-oem/](https://plot.ly/products/consulting-and-oem/).
 
         ***
-
-        ### Why Dash?
-
-        Learn more about dash from our talk at
-        [Plotcon](https://plotcon.plot.ly).
 
     '''.replace('  ', '')),
 
