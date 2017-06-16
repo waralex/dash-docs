fc43a23ddf20be38f98537cd319df602d5fe8129
chriddyp <chris@plot.ly>
2017-06-15 22:43:48 -0400

    formatting

--- tutorial/getting_started.py
+++ tutorial/getting_started.py
@@ -299,33 +299,32 @@ layout = html.Div([
     '''.replace('    ', '')),
 
     html.Div(
-        dcc.Markdown('''
-            >>> help(dcc.Dropdown)
-            class Dropdown(dash.development.base_component.Component)
-            |  A Dropdown component.
-            |  Dropdown is an interactive dropdown element for selecting one or more
-            |  items.
-            |  The values and labels of the dropdown items are specified in the `options`
-            |  property and the selected item(s) are specified with the `value` property.
-            |
-            |  Use a dropdown when you have many options (more than 5) or when you are
-            |  constrained for space. Otherwise, you can use RadioItems or a Checklist,
-            |  which have the benefit of showing the users all of the items at once.
-            |
-            |  Keyword arguments:
-            |  - id (string; optional)
-            |  - className (string; optional)
-            |  - disabled (boolean; optional): If true, the option is disabled
-            |  - multi (boolean; optional): If true, the user can select multiple values
-            |  - options (list; optional)
-            |  - placeholder (string; optional): The grey, default text shown when no option is selected
-            |  - value (string | list; optional): The value of the input. If `multi` is false (the default)
-            |  then value is just a string that corresponds to the values
-            |  provided in the `options` property. If `multi` is true, then
-            |  multiple values can be selected at once, and `value` is an
-            |  array of items with values corresponding to those in the
-            |  `options` prop.
-        '''.replace('    ', '')), style=styles.code_container),
+        dcc.Markdown('''```
+>>> help(dcc.Dropdown)
+class Dropdown(dash.development.base_component.Component)
+|  A Dropdown component.
+|  Dropdown is an interactive dropdown element for selecting one or more
+|  items.
+|  The values and labels of the dropdown items are specified in the `options`
+|  property and the selected item(s) are specified with the `value` property.
+|
+|  Use a dropdown when you have many options (more than 5) or when you are
+|  constrained for space. Otherwise, you can use RadioItems or a Checklist,
+|  which have the benefit of showing the users all of the items at once.
+|
+|  Keyword arguments:
+|  - id (string; optional)
+|  - className (string; optional)
+|  - disabled (boolean; optional): If true, the option is disabled
+|  - multi (boolean; optional): If true, the user can select multiple values
+|  - options (list; optional)
+|  - placeholder (string; optional): The grey, default text shown when no option is selected
+|  - value (string | list; optional): The value of the input. If `multi` is false (the default)
+|  then value is just a string that corresponds to the values
+|  provided in the `options` property. If `multi` is true, then
+|  multiple values can be selected at once, and `value` is an
+|  array of items with values corresponding to those in the
+|  `options` prop.```'''), style=styles.code_container),
 
     dcc.Markdown('''
         ### Summary
