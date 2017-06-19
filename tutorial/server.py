780b8970c85271f4ff73ce4a651c316a06c7f403
chriddyp <chris@plot.ly>
2017-06-19 15:44:14 -0400

    debug cookies

--- tutorial/server.py
+++ tutorial/server.py
@@ -3,7 +3,7 @@ import dash
 from dash import Dash
 import dash_html_components as html
 import dash_core_components as dcc
-from flask import Flask
+from flask import Flask, request
 import json
 import plotly.graph_objs as go
 import os
@@ -11,5 +11,11 @@ import os
 server = Flask(__name__)
 server.secret_key = os.environ.get('secret_key', 'secret')
 app = Dash(__name__, server=server, url_base_pathname='/dash/')
+
+def print_cookies():
+    print request.headers
+    print request.cookies
+
+server.before_request(print_cookies)
 
 app.config.supress_callback_exceptions = True
