040a43ef16923c93ed82ecd3340ade1a8df40e9b
Eric O. LEBIGOT (EOL) <lebigot@users.noreply.github.com>
2017-06-23 00:19:06 +0200

    Fixed officially discouraged style
    Python efficiently creates a single string literal from multiple consecutive ones. This is officially recommended, compared to adding strings, which is wasteful (multiple strings created in memory, that are concatenated one by one into bigger and bigger strings).

--- tutorial/examples/getting_started_table.py
+++ tutorial/examples/getting_started_table.py
@@ -5,9 +5,9 @@ import dash_html_components as html
 import pandas as pd
 
 df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/' +
-    'c78bf172206ce24f77d6363a2d754b59/raw/' +
-    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/' +
+    'https://gist.githubusercontent.com/chriddyp/'
+    'c78bf172206ce24f77d6363a2d754b59/raw/'
+    'c353e8ef842413cae56ae3920b8fd78468aa4cb2/'
     'usa-agricultural-exports-2011.csv')
 
 def generate_table(dataframe, max_rows=10):
