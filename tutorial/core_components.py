c74a5a10ff95d4d462b6c3a5f523e99c73a0326c
alishobeiri <alishobeiri@ymail.com>
2017-08-08 13:03:58 -0400

    Changed datetime.datetime to dt

--- tutorial/core_components.py
+++ tutorial/core_components.py
@@ -59,16 +59,16 @@ dcc.Dropdown(
     html.Hr(),
     html.H3('Date Picker'),
     dcc.SyntaxHighlighter('''import dash_core_components as dcc
-    from datetime import *
+    from datetime import datetime as dt
 
 dcc.DatePickerSingle(
     id='date-picker-single',
-    date=dt.datetime(1997, 5, 10)
+    date=dt(1997, 5, 10)
 ),
 
 dcc.DatePickerRange(
     id='date-picker-range',
-    start_date=dt.datetime(1997, 5, 3),
+    start_date=dt(1997, 5, 3),
     end_date_placeholder_text='Select a date!'
 )
 ''', language='python', customStyle=styles.code_container),
@@ -84,13 +84,13 @@ dcc.DatePickerRange(
     ),
 
     dcc.SyntaxHighlighter('''import dash_core_components as dcc
-    import datetime
+    from datetime import datetime as dt
 
     dcc.DatePickerSingle(
         id='date-picker-single',
-        initial_visible_month=datetime.datetime(1997, 5, 5),
-        min_date_range=datetime.datetime(1997, 4, 29),
-        max_date_range=datetime.datetime(1997, 6, 3),
+        initial_visible_month=dt(1997, 5, 5),
+        min_date_range=dt(1997, 4, 29),
+        max_date_range=dt(1997, 6, 3),
         show_outside_days=True,
         with_portal=True,
         number_of_months_shown=1,
@@ -99,12 +99,12 @@ dcc.DatePickerRange(
 
     dcc.DatePickerRange(
         id='date-picker-range',
-        start_date=datetime.datetime(1997, 5, 10),
+        start_date=dt(1997, 5, 10),
         end_date_placeholder_text="Clear the date!",
         first_day_of_week=3,
         minimum_nights=2,
-        min_date_range=datetime.datetime(1997, 4, 29),
-        max_date_range=datetime.datetime(1997, 6, 3),
+        min_date_range=dt(1997, 4, 29),
+        max_date_range=dt(1997, 6, 3),
         show_outside_days=True,
         with_portal=True,
         number_of_months_shown=2,
@@ -116,9 +116,9 @@ dcc.DatePickerRange(
     )''', language='python', customStyle=styles.code_container),
     dcc.DatePickerSingle(
         id='section2-datepickersingle-2',
-        initial_visible_month=datetime.datetime(1997, 5, 5),
-        min_date_range=datetime.datetime(1997, 4, 29),
-        max_date_range=datetime.datetime(1997, 6, 3),
+        initial_visible_month=dt(1997, 5, 5),
+        min_date_range=dt(1997, 4, 29),
+        max_date_range=dt(1997, 6, 3),
         show_outside_days=True,
         with_portal=True,
         number_of_months_shown=1,
@@ -127,12 +127,12 @@ dcc.DatePickerRange(
 
     dcc.DatePickerRange(
         id='section2-datepickerrange-2',
-        start_date=datetime.datetime(1997, 5, 10),
+        start_date=dt(1997, 5, 10),
         end_date_placeholder_text="Clear the date!",
         first_day_of_week=3,
         minimum_nights=2,
-        min_date_range=datetime.datetime(1997, 4, 29),
-        max_date_range=datetime.datetime(1997, 6, 3),
+        min_date_range=dt(1997, 4, 29),
+        max_date_range=dt(1997, 6, 3),
         show_outside_days=True,
         with_portal=True,
         number_of_months_shown=2,
