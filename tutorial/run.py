f8fc342ea34fdc47e5d908152bb498a59060a1ec
chriddyp <chris@plot.ly>
2017-08-15 16:33:36 -0500

    fix cdn

--- tutorial/run.py
+++ tutorial/run.py
@@ -250,7 +250,7 @@ app.css.append_css({
 })
 
 if 'DYNO' in os.environ:
-    app.scripts.config.serve_locally = False
+    app.scripts.config.serve_locally = True
     app.scripts.append_script({
         'external_url': 'https://cdn.rawgit.com/chriddyp/ca0d8f02a1659981a0ea7f013a378bbd/raw/e79f3f789517deec58f41251f7dbb6bee72c44ab/plotly_ga.js'
     })
