6e17b912f34bb3a40f83f893a1f802e189ef6fea
cldougl <cldougl@gmail.com>
2017-09-08 14:55:14 -0400

    add workshop info

--- tutorial/run.py
+++ tutorial/run.py
@@ -249,6 +249,7 @@ header = html.Div(
 
             html.Div(className="links", children=[
                 html.A('pricing', className="link", href="https://plot.ly/products/on-premise"),
+                html.A('workshop', className="link", href="https://plotcon.plot.ly/workshops"),
                 html.A('user guide', className="link active", href="https://plot.ly/dash/"),
                 html.A('plotly', className="link", href="https://plot.ly/")
             ])
