ae2ba14cc858f063c9c55c0cd4d6596f185c27ab
chriddyp <chris@plot.ly>
2017-09-13 21:07:40 -0400

    update graph example

--- tutorial/core_components.py
+++ tutorial/core_components.py
@@ -336,13 +336,35 @@ import plotly.graph_objs as go
 dcc.Graph(
     figure=go.Figure(
         data=[
-            go.Scatter(x=[1, 2, 3], y=[3, 2, 4], mode='lines'),
-            go.Scatter(x=[1, 2, 3], y=[4, 1, 5], mode='lines')
+            go.Bar(
+                x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                   2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                y=[219, 146, 112, 127, 124, 180, 236, 207, 236, 263,
+                   350, 430, 474, 526, 488, 537, 500, 439],
+                name='Rest of world',
+                marker=go.Marker(
+                    color='rgb(55, 83, 109)'
+                )
+            ),
+            go.Bar(
+                x=[1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
+                   2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012],
+                y=[16, 13, 10, 11, 28, 37, 43, 55, 56, 88, 105, 156, 270,
+                   299, 340, 403, 549, 499],
+                name='China',
+                marker=go.Marker(
+                    color='rgb(26, 118, 255)'
+                )
+            )
         ],
         layout=go.Layout(
-            title='Quarterly Results',
-            showlegend=False,
-            margin=go.Margin(l=20, r=0, t=40, b=20)
+            title='US Export of Plastic Scrap',
+            showlegend=True,
+            legend=go.Legend(
+                x=0,
+                y=1.0
+            ),
+            margin=go.Margin(l=40, r=0, t=40, b=30)
         )
     ),
     style={'height': 300},
