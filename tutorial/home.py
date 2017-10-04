f66e1a92008e4cb81db55b9c9c02b7edcc14ef70
chriddyp <chris@plot.ly>
2017-10-03 22:36:40 -0400

    refactor home page links

--- tutorial/home.py
+++ tutorial/home.py
@@ -1,5 +1,9 @@
+# -*- coding: utf-8 -*-
 import dash_core_components as dcc
 import dash_html_components as html
+from textwrap import dedent as s
+
+from tools import merge
 
 styles = {
     'underline': {
@@ -8,94 +12,117 @@ styles = {
     }
 }
 
+
+def Chapter(name, href=None, caption=None):
+    linkComponent = html.A if href.startswith('http') else dcc.Link
+    return html.Div([
+        html.Li(linkComponent(name, href=href, style={'paddingLeft': 0})),
+        html.Small(dcc.Markdown(s(caption or '')), style={
+            'display': 'block',
+            'marginTop': '-10px' if caption else ''
+        }) if caption else None
+    ])
+
+
+
+def Section(title, links, description=None, headerStyle={}):
+    return html.Div([
+        html.H2(title, style=merge(styles['underline'], headerStyle)),
+        (
+            html.Div(description)
+            if description is not None else None
+        ),
+        html.Ul(links)
+    ])
+
+
 layout = html.Div(className="toc", children=[
     html.H1('Dash User Guide'),
 
-    html.H2("What's Dash?", style=styles['underline']),
+    Section("What's Dash?", [
+        Chapter('Introduction', '/dash/introduction'),
+        Chapter('Announcement', 'https://medium.com/@plotlygraphs/introducing-dash-5ecf7191b503'),
+        Chapter('Dash App Gallery', '/dash/gallery')
+    ]),
 
-    html.Div([html.Ul([
-
-        html.Li(
-            dcc.Link(html.A('Introduction'), href="/dash/introduction")
+    Section('Dash Tutorial', [
+        Chapter('Part 1. Installation', '/dash/installation'),
+        Chapter(
+            'Part 2. The Dash Layout',
+            '/dash/getting-started',
+            '''The Dash `layout` describes what your app will
+            look like and is composed of a set of declarative Dash components.
+        '''),
+        Chapter(
+            'Part 3. Basic Callbacks',
+            '/dash/getting-started-part-2',
+            '''Dash apps are made interactive through Dash Callbacks:
+            Python functions that are automatically called whenver an input
+            component's property changes. Callbacks can be chained, allowing
+            one update in the UI to trigger several updates across the app.'''
         ),
-
-        html.Li(
-            html.A('Announcement Letter',
-                   href="https://medium.com/@plotlygraphs/introducing-dash-5ecf7191b503"),
+        Chapter(
+            'Part 4. Callbacks With State',
+            '/dash/state',
+            '''Basic callbacks are fired whenever the values change. Use
+            Dash `State` with Dash `Inputs` to pass in extra values whenever
+            the `Inputs` change. `State` is useful for UIs that contain
+            forms or buttons.'''
         ),
-
-        html.Li(
-            dcc.Link(html.A('Dash App Gallery'), href="/dash/gallery"),
-        ),
-    ])], className=""),
-
-    html.H2("Creating Your First App", style=styles['underline']),
-
-    html.Div([html.Ul([
-        html.Li(dcc.Link(html.A('Installation'), href="/dash/installation")),
-
-        html.Li(
-            dcc.Link(
-                html.A('Dash Layout'),
-                href="/dash/getting-started"
-            )
-        ),
-
-        html.Li(
-            dcc.Link(
-                html.A('Dash Callbacks'),
-                href="/dash/getting-started-part-2"
-            )
+        Chapter(
+            'Part 5. Interactive Graphing and Crossfiltering',
+            '/dash/interactive-graphing',
+            '''Bind interactivity to the Dash `Graph` component whenever you
+            hover, click, or select points on your chart.'''
         )
-    ])], className=""),
+    ]),
 
-    html.H2('Component Libraries', style=styles['underline']),
-    html.Div([html.Ul([
-        html.Li(dcc.Link(html.A('Dash Core Components'), href="/dash/dash-core-components")),
+    Section('Component Libraries', [
+        Chapter('Dash Core Components', '/dash/dash-core-components', '''
+            The Dash Core Component library contains a set of higher-level
+            components like sliders, graphs, dropdowns, tables, and more.
+        '''),
+        Chapter('Dash HTML Components', '/dash/dash-html-components', '''
+            Dash provides all of the available HTML tags as user-friendly
+            Python classes. This chapter explains how this works and the few
+            important key differences between Dash HTML components and standard
+            html.
+        '''),
+        Chapter('Build Your Own Components', '/dash/plugins', '''
+            Dash components are built with [React.js](https://reactjs.org/).
+            Dash provides a React → Dash toolchain that generates a
+            Dash-compatible interface to these components in Python.
+        ''')
+    ]),
 
-        html.Li(dcc.Link(html.A('Dash HTML Components'), href="/dash/dash-html-components")),
+    Section('Advanced Usage', [
+        Chapter('Performance', '/dash/performance'),
+        Chapter('Live Updates', '/dash/live-updates'),
+        Chapter('External CSS and JS', '/dash/external-resources'),
+        Chapter('URL Routing and Multiple Apps', '/dash/urls')
+    ]),
 
-        html.Li(dcc.Link(html.A('Build Your Own Components'), href="/dash/plugins")),
-    ])], className=""),
+    Section('Production', [
+        Chapter('Authentication', '/dash/authentication'),
+        Chapter('Deployment', '/dash/deployment'),
+    ]),
 
-    html.H2('Advanced Usage', style=styles['underline']),
-    html.Div([html.Ul([
-        html.Li(
-            dcc.Link(html.A('Performance'), href="/dash/performance"),
+    Section('Getting Help', [
+        Chapter('FAQ', 'https://community.plot.ly/c/dash'),
+        Chapter('Support and Contact', href="/dash/support")
+    ]),
+
+    Section('Plotly On-Premises', [
+        Chapter(
+            'About Plotly On-Premises',
+            'https://plot.ly/products/on-premise'
         ),
-
-        html.Li(
-            dcc.Link(html.A('Live Updates'), href="/dash/live-updates"),
-        ),
-
-        html.Li(
-            dcc.Link(
-                html.A('External CSS and JS'),
-                href="/dash/external-resources"
-            )
-        ),
-
-        html.Li(
-            dcc.Link(
-                html.A('URL Routing and Multiple Apps'),
-                href="/dash/urls"
-            )
-        )
-    ])], className=''),
-
-    html.H2('Production', style=styles['underline']),
-
-    html.Div(html.Ul([
-        html.Li(dcc.Link(html.A('Authentication'), href="/dash/authentication")),
-
-        html.Li(dcc.Link(html.A('Deployment'), href="/dash/deployment")),
-
-    ]), className=""),
-
-    html.H2('Getting Help', style=styles['underline']),
-    html.Div([html.Ul([
-        html.Li(html.A('FAQ', href="https://community.plot.ly/c/dash")),
-
-        html.Li(dcc.Link(html.A('Support and Contact'), href="/dash/support"))
-    ])], className="")
+        Chapter(
+            'Deploying Dash Apps on Plotly On-Premises',
+            '/dash/deployment/on-premise'
+        )],
+        description="""Plotly On-Premises is Plotly's commercial offering for
+        hosting and sharing Dash apps.""",
+        headerStyle={'color': '#0D76BF'}
+    )
 ])
