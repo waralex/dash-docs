94ebb47ed6fe354a469968bfda3adf68761e2c4f
chriddyp <chris@plot.ly>
2017-10-04 17:04:01 -0400

    generic crossfiltering use case

--- tutorial/graphing.py
+++ tutorial/graphing.py
@@ -10,6 +10,9 @@ examples = {
         'tutorial/examples/graph_callbacks_simple.py'),
     'world-indicators': load_example(
         'tutorial/examples/getting_started_crossfilter.py'
+    ),
+    'crossfilter-recipe': load_example(
+        'tutorial/examples/crossfilter_recipe.py'
     )
 }
 
@@ -52,13 +55,69 @@ layout = html.Div([
     """),
     Example(examples['simple-graph-events'][1]),
 
+    html.Hr(),
+
+    html.H3('Update Graphs on Hover'),
+
     Syntax(examples['world-indicators'][0], summary="""
-    Let's update our world indicators example by displaying time series when
-    we hover over points in our scatter plot.
+    Let's update our world indicators example from the previous chapter
+    by updating time series when we hover over points in our scatter plot.
     """),
     Example(examples['world-indicators'][1]),
 
     dcc.Markdown(s('''
+    Try mousing over the points in the scatter plot on the left.
+    Notice how the line graphs on the right update based off of the point that
+    you are hovering over.
+    ''')),
+
+    html.Hr(),
+
+    html.H3('Generic Crossfilter Recipe'),
+
+    Syntax(examples['crossfilter-recipe'][0], summary="""
+    Here's a slightly more generic example for crossfiltering across
+    a six column data set. Each scatter plot's selection filters the
+    underlying dataset.
+    """),
+
+    Example(examples['crossfilter-recipe'][1]),
+
+    dcc.Markdown(s('''
+    Try clicking and dragging in any of the plots to filter different regions.
+    On every selection, the three graph callbacks are fired with the latest
+    selected regions of each plot. A pandas dataframe is fitered based off
+    of the selected points and the graphs are replotted with the selected
+    points highlighted and the selected region drawn as a dashed rectangle.
+
+    > As an aside, if you find yourself filtering and visualizing
+    highly-dimensional datasets, you should consider checking out the
+    [parallel coordinates](https://plot.ly/python/parallel-coordinates-plot/)
+    chart type.
+    ''')),
+
+    dcc.Markdown(s('''
+
+    ### Current Limitations
+
+    There are a few limitations in graph interactions right now.
+    - Box select and lasso select are only supported for scatter plots.
+      As a workaround, you can enable selection on other chart types
+      (like bar charts) by drawing an invisible scatter trace.
+      This isssue is being worked on in [https://github.com/plotly/plotly.js/issues/170](https://github.com/plotly/plotly.js/issues/170).
+    - Clicking points does not accumulate: you cannot accumulate the number
+      of points that you have clicked on nor is there the concept of
+      "unselecting" a point. This issue is being worked on in [https://github.com/plotly/plotly.js/issues/1848](https://github.com/plotly/plotly.js/issues/1848).
+    - It is not currently possible to customize the style of the hover
+      interactions or the select box.
+      This issue is being worked on in [https://github.com/plotly/plotly.js/issues/1847](https://github.com/plotly/plotly.js/issues/1847).
+
+    ***
+
+    There's a lot that you can do with these interactive plotting features.
+    If you need help exploring your use case, open up a thread in the
+    [Dash Community Forum](https://community.plot.ly/c/dash).
+
     ***
 
     The final chapter of the Dash tutorial explains one last concept of dash:
