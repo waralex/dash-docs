b50bec707599608847c56b7c6667a40150b57d51
chriddyp <chris@plot.ly>
2018-02-13 16:31:52 -0500

    cors

--- tutorial/server.py
+++ tutorial/server.py
@@ -7,11 +7,20 @@ from flask import Flask, request
 import json
 import plotly.graph_objs as go
 import os
+from flask_cors import CORS
 
 server = Flask(__name__, static_url_path='/dash/static', static_folder='./static')
 server.secret_key = os.environ.get('secret_key', 'secret')
-app = Dash(__name__, server=server, url_base_pathname='/dash/', csrf_protect=False)
+app = Dash(
+    __name__,
+    server=server,
+    csrf_protect=False,
+    url_base_pathname='/dash/'
+)
+# app.config.requests_pathname_prefix = 'https://dash-docs.herokuapp.com/dash/'
+# app.config.routes_pathname_prefix = '/dash/'
+
 app.css.config.serve_locally = False
 app.scripts.config.serve_locally = False
-
 app.config.suppress_callback_exceptions = True
+CORS(app.server)
