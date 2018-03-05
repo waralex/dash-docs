81a59eaa58aa4ce9da3bf4edbb9b729edf4ee447
Chris Parmer <chris@plot.ly>
2018-03-05 15:22:42 -0500

    use `selected` attributes (#75)
    * use `selected` attributes
    
    we can’t use a new trace to style properties because of
    https://github.com/plotly/plotly.js/issues/2298
    
    * bump version

--- tutorial/examples/crossfilter_recipe.py
+++ tutorial/examples/crossfilter_recipe.py
@@ -10,28 +10,24 @@ app = dash.Dash()
 
 np.random.seed(0)
 df = pd.DataFrame({
-    'Column {}'.format(i): np.random.rand(50) + i*10
+    'Column {}'.format(i): np.random.rand(30) + i*10
     for i in range(6)})
 
 app.layout = html.Div([
     html.Div(
         dcc.Graph(
             id='g1',
-            # if selectedData is not specified then it is initialized as None
-            selectedData={'points': [], 'range': None},
             config={'displayModeBar': False}
         ), className='four columns'
     ),
     html.Div(
         dcc.Graph(
             id='g2',
-            selectedData={'points': [], 'range': None},
             config={'displayModeBar': False}
         ), className='four columns'),
     html.Div(
         dcc.Graph(
             id='g3',
-            selectedData={'points': [], 'range': None},
             config={'displayModeBar': False}
         ), className='four columns')
 ], className='row')
@@ -39,45 +35,56 @@ app.layout = html.Div([
 
 def highlight(x, y):
     def callback(*selectedDatas):
-        index = df.index
+        selectedpoints = df.index
+        for i, selected_data in enumerate(selectedDatas):
+            if selected_data is not None:
+                selected_index = [
+                    p['customdata'] for p in selected_data['points']
+                ]
+                if len(selected_index) > 0:
+                    selectedpoints = np.intersect1d(
+                        selectedpoints, selected_index)
 
-        # filter the dataframe by the selected points
-        for i, hover_data in enumerate(selectedDatas):
-            selected_index = [
-                p['customdata'] for p in selectedDatas[i]['points']
-                # the first trace that includes all the data
-                if p['curveNumber'] == 0
-            ]
-            if len(selected_index) > 0:
-                index = np.intersect1d(index, selected_index)
 
-        dff = df.iloc[index, :]
+        # set which points are selected with the `selectedpoints` property
+        # and style those points with the `selected` and `unselected`
+        # attribute. see
+        # https://medium.com/@plotlygraphs/notes-from-the-latest-plotly-js-release-b035a5b43e21
+        # for an explanation
 
-        color = 'rgb(125, 58, 235)'
-
-        trace_template = {
-            'marker': {
-                'color': color,
-                'size': 12,
-                'line': {'width': 0.5, 'color': 'white'}
-            }
-        }
         figure = {
             'data': [
-                # the first trace displays all of the points
-                # it is dimmed by setting opacity to 0.1
-                dict({
-                    'x': df[x], 'y': df[y], 'text': df.index,
+                {
+                    'x': df[x],
+                    'y': df[y],
+                    'text': df.index,
+                    'textposition': 'top',
+                    'selectedpoints': selectedpoints,
                     'customdata': df.index,
-                    'mode': 'markers', 'opacity': 0.1
-                }, **trace_template),
-
-                # the second trace is plotted on top of the first trace and
-                # displays the filtered points
-                dict({
-                    'x': dff[x], 'y': dff[y], 'text': dff.index,
-                    'mode': 'markers+text', 'textposition': 'top',
-                }, **trace_template),
+                    'type': 'scatter',
+                    'mode': 'markers+text',
+                    'textposition': 'top',
+                    'marker': {
+                        'color': 'rgba(0, 116, 217, 0.7)',
+                        'size': 12,
+                        'line': {
+                            'color': 'rgb(0, 116, 217)',
+                            'width': 0.5
+                        }
+                    },
+                    'textfont': {
+                        'color': 'rgba(30, 30, 30, 1)'
+                    },
+                    'unselected': {
+                        'marker': {
+                            'opacity': 0.3,
+                        },
+                        'textfont': {
+                            # make text transparent when not selected
+                            'color': 'rgba(0, 0, 0, 0)'
+                        }
+                    }
+                },
             ],
             'layout': {
                 'margin': {'l': 15, 'r': 0, 'b': 15, 't': 5},
@@ -96,7 +103,7 @@ def highlight(x, y):
                 'color': 'darkgrey'
             }
         }
-        if selectedDatas[0]['range']:
+        if selectedDatas[0] and selectedDatas[0]['range']:
             figure['layout']['shapes'] = [dict({
                 'x0': selectedDatas[0]['range']['x'][0],
                 'x1': selectedDatas[0]['range']['x'][1],
