f6e35df47d6d6808745edea726ed1f318dbc0ea2
Chris P <chris@plot.ly>
2018-08-13 12:06:04 -0400

    :snake: python doesn't require parantheses

--- tutorial/examples/core_components/tabs_callback.py
+++ tutorial/examples/core_components/tabs_callback.py
@@ -17,11 +17,11 @@ app.layout = html.Div([
 @app.callback(Output('tabs-content', 'children'),
               [Input('tabs', 'value')])
 def render_content(tab):
-    if(tab == 'tab-1'):
+    if tab == 'tab-1':
         return html.Div([
             html.H3('Tab content 1')
         ])
-    elif(tab == 'tab-2'):
+    elif tab == 'tab-2':
         return html.Div([
             html.H3('Tab content 2')
         ])
