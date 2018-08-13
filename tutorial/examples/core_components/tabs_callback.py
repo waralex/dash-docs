a48de6feb7db57b93b7188744561415bc6a0e312
Chris P <chris@plot.ly>
2018-08-13 12:05:48 -0400

    :triangular_ruler: use the shorter imports

--- tutorial/examples/core_components/tabs_callback.py
+++ tutorial/examples/core_components/tabs_callback.py
@@ -14,8 +14,8 @@ app.layout = html.Div([
     html.Div(id='tabs-content')
 ])
 
-@app.callback(dash.dependencies.Output('tabs-content', 'children'),
-            [dash.dependencies.Input('tabs', 'value')])
+@app.callback(Output('tabs-content', 'children'),
+              [Input('tabs', 'value')])
 def render_content(tab):
     if(tab == 'tab-1'):
         return html.Div([
