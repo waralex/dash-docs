e3a1182c3480de615048297f63da4baaeac0a174
Chris P <chris@plot.ly>
2018-08-13 12:05:32 -0400

    :one: select the first tab by default
    this is what our users will most likely want to do

--- tutorial/examples/core_components/tabs_callback.py
+++ tutorial/examples/core_components/tabs_callback.py
@@ -7,7 +7,7 @@ from dash.dependencies import Input, Output
 app = dash.Dash()
 
 app.layout = html.Div([
-    dcc.Tabs(id="tabs", value='tab-2', children=[
+    dcc.Tabs(id="tabs", value='tab-1', children=[
         dcc.Tab(label='Tab one', value='tab-1'),
         dcc.Tab(label='Tab two', value='tab-2'),
         ]),
