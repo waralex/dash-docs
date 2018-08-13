f3fce666384d8072bd4968b3a62ffdcf05151f0d
Chris P <chris@plot.ly>
2018-08-13 12:06:21 -0400

    :space_invader: consistent formatting

--- tutorial/examples/core_components/tabs_callback.py
+++ tutorial/examples/core_components/tabs_callback.py
@@ -10,7 +10,7 @@ app.layout = html.Div([
     dcc.Tabs(id="tabs", value='tab-1', children=[
         dcc.Tab(label='Tab one', value='tab-1'),
         dcc.Tab(label='Tab two', value='tab-2'),
-        ]),
+    ]),
     html.Div(id='tabs-content')
 ])
 
