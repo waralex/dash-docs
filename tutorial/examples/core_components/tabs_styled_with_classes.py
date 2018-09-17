d9916419beed07660cf9091b1b338852ea19fc43
Valentijn Nieman <valentijnnieman@gmail.com>
2018-09-17 17:51:11 -0400

    Fixed bug caused by having two Tabs components without callback method

--- tutorial/examples/core_components/tabs_styled_with_classes.py
+++ tutorial/examples/core_components/tabs_styled_with_classes.py
@@ -7,15 +7,24 @@ from dash.dependencies import Input, Output
 app = dash.Dash()
 
 app.layout = html.Div([
-    dcc.Tabs(id="tabs-with-classes", parent_className='custom-tabs', content_className='custom-tabs-content', className='custom-tabs-container', children=[
-        dcc.Tab(label='Tab one', className='custom-tab', selected_className='custom-tab--selected', children=[
-            html.H1("Styled content with CSS classes")
+    dcc.Tabs(id="tabs-with-classes", value='tab-1', parent_className='custom-tabs', className='custom-tabs-container', children=[
+        dcc.Tab(label='Tab one', value='tab-1', className='custom-tab', selected_className='custom-tab--selected'),
+        dcc.Tab(label='Tab two', value='tab-2', className='custom-tab', selected_className='custom-tab--selected'),
     ]),
-        dcc.Tab(label='Tab two', className='custom-tab', selected_className='custom-tab--selected', children=[
-            html.H1("Styled content with CSS classes part 2!")
-        ]),
-    ]),
+    html.Div(id='tabs-content-classes')
 ])
+
+@app.callback(Output('tabs-content-classes', 'children'),
+              [Input('tabs-with-classes', 'value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return html.Div([
+            html.H3('Tab content 1')
+        ])
+    elif tab == 'tab-2':
+        return html.Div([
+            html.H3('Tab content 2')
+        ])
 
 
 if __name__ == '__main__':
