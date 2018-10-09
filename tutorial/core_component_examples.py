188ba96a0fb91120ba9256dc34ee10138868dfef
bcdunbar <branden.dunbar@gmail.com>
2018-10-09 21:32:16 +1100

    add btn content

--- tutorial/core_component_examples.py
+++ tutorial/core_component_examples.py
@@ -28,6 +28,8 @@ examples = {
     'tabs_styled_with_classes_css':  tools.read_file('assets/tabs-styled-with-classes.css'),
     'tabs_styled_with_inline':  tools.load_example('tutorial/examples/core_components/tabs_styled_with_inline.py'),
     'tabs_styled_with_props':  tools.load_example('tutorial/examples/core_components/tabs_styled_with_props.py'),
+    'button_basic': tools.load_example('tutorial/examples/core_components/button_basic.py'),
+    'button_n_clicks_timestamp': tools.load_example('tutorial/examples/core_components/button_n_clicks_timestamp.py')
 }
 
 
@@ -412,12 +414,22 @@ RadioItems = html.Div(children=[
 
 # Button
 Button = html.Div(children=[
-    html.H3('Button Examples'),
+    html.H1('Button Examples and Reference'),
     html.Hr(),
-    
+    html.Br(),
+    html.H3('Button Basic Example'),
+    html.Hr(),
+    Syntax(examples['button_basic'][0]),
+    Example(examples['button_basic'][1]),
+    html.Br(),
+    html.H3('Button with n_clicks_timestamp'),
+    html.Hr(),
+    Syntax(examples['button_n_clicks_timestamp'][0]),
+    Example(examples['button_n_clicks_timestamp'][1]),
+    html.Br(),
     html.H3('Button Properties'),
     html.Hr(),
-    # generate_prop_table('Button')
+    generate_prop_table('Button')
 ])
 
 
