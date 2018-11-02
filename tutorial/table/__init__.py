9b1c61913d3e16c9d4ee542bb47721a036b6f00c
cldougl <cldougl@gmail.com>
2018-11-02 15:16:53 -0400

    :see_no_evil: init typo

--- tutorial/table/__init__.py
+++ tutorial/table/__init__.py
@@ -1,6 +1,6 @@
 from .import dropdowns_chapter
 from .import editing_recipes_chapter
-from . import filtering_chapter
+from .import filtering_chapter
 from .import fixed_rows_chapter
 from .import interactivity_chapter
 from .import reference_chapter
