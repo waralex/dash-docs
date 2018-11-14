ad83396d97270e6f561263ff390eed3f38d83ec2
matthewchan <matthew.chan@shaw.ca>
2018-11-13 17:01:45 -0700

    Changed Dash DAQ URL from Heroku -> DDS

--- tutorial/gallery.py
+++ tutorial/gallery.py
@@ -442,7 +442,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Dash DataTable',
             app_link='/datatable',
             code_link='https://github.com/plotly/dash-table',
-            img_src='https://raw.githubusercontent.com/plotly/datatable-experiments/master/images/DataTable.gif',
+            img_src='https://github.com/plotly/dash-table-experiments/raw/master/images/DataTable.gif',
             description='''
         Dash provides an interactive `DataTable` as part of the `data-table`
         project. This table includes built-in filtering, row-selection,
@@ -500,7 +500,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Omega CN32PT-440-DC PID Controller',
             app_link='https://www.dashdaq.io/operate-an-omega-cn32pt-440-dc-pid-controller-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-omega-pid.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-omega-pid',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/07/Dash-DAQ-Omega-Platnium-Controller-146@05x.jpg',
             description='''
             Let’s heat things up with Dash DAQ! With this application, we use
@@ -513,7 +513,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Wireless Arduino Robot',
             app_link='https://www.dashdaq.io/control-a-wireless-arduino-robot-in-python',
             code_name_display_text='Try the app',
-            code_link='http://dash-daq-sparki.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-sparki',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/07/Dash-DAQ-Sparki-11-Edit@05x.jpg',
             description='''
             We love our robots here at Plotly! This Dash DAQ app wirelessly
@@ -527,7 +527,7 @@ layout = html.Div(className='gallery', children=[
             app_name='I-V curve tracer with a Keithley 2400 SourceMeter',
             app_link='https://www.dashdaq.io/build-an-i-v-curve-tracer-with-a-keithley-2400-sourcemeter-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-iv-tracer.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-iv-tracer',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/07/IMG_0345@05x.jpg',
             description='''
             With this Dash DAQ application written in Python, you can create
@@ -539,7 +539,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Control a Robotic Arm',
             app_link='https://www.dashdaq.io/control-a-robotic-arm-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-robotic-arm-edge.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-robotic-arm',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/07/Dash-DAQ-Robotic-Arm-Edge-19@05x.jpg',
             description='''
             Dash DAQ’s GUI components let you interface with all the robot’s
@@ -554,7 +554,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Ocean Optics Spectrometer',
             app_link='https://www.dashdaq.io/control-an-ocean-optics-spectrometer-in-python',
             code_name_display_text='Try the app',
-            code_link='http://dash-ocean-optics.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-ocean-optics',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/06/Dash-DAQ-_-Ocean-Optics-2@05x.jpg',
             description='''
             We wrote a Dash DAQ application in Python to
@@ -566,7 +566,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Kurt J. Lesker Pressure Gauge Controller',
             app_link='https://www.dashdaq.io/read-pressure-from-kurt-j-lesker-gauge-controller-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-pressure-gauge-kjl.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-pressure-gauge-kjl',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/06/Dash-DAQ-Kurt@05x.jpg',
             description='''
             A Dash DAQ application, written in Python, gives you clean, modern
@@ -581,7 +581,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Read Accelerometer Data',
             app_link='https://www.dashdaq.io/read-phidgets-accelerometer-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-accelerometer.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-accelerometer',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/06/Dash-DAQ-_-Accelerometer@05x.jpg',
             description='''
             Running tests with an accelerometer? Dash DAQ gives you the
@@ -593,7 +593,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Control an LED Strip',
             app_link='https://www.dashdaq.io/control-an-led-strip-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-led.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-led',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/05/Dash-DAQ-_-Blank-Stick-main@05x.jpg',
             description='''
             Team Plotly is getting colorful with Dash DAQ! This application
@@ -608,7 +608,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Control a Stepper Motor',
             app_link='https://www.dashdaq.io/stepper-motor-control-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-stepper-motor.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-stepper-motor',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/04/Dash-DAQ-_-Stepper-Motor@05x.jpg',
             description='''
             From 3D printers, to mirror mounts, to machine tools – stepper
@@ -620,7 +620,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Tektronix Oscilloscope Data Logging',
             app_link='https://www.dashdaq.io/oscilloscope-logging-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-tektronix-350.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-tektronix350',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/04/Dash-DAQ-_-Tektronix-Oscilloscope-Tektronic-Function-Generator@05x-1.jpg',
             description='''
             Whether testing your power supply or monitoring a heartbeat, if
@@ -647,7 +647,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Agilent 34401A Multimeter',
             app_link='https://www.dashdaq.io/read-agilent-34401a-multimeter-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-hp-multimeter.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-hp-multimeter',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/03/Dash-DAQ-_-Multimeter-zoom@05x.jpg',
             description='''
             Here’s how we used Dash DAQ’s interactive UI components to
@@ -661,7 +661,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Tektronix Function Generator',
             app_link='https://www.dashdaq.io/control-tektronix-function-generator-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-tektronix-350.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-tektronix350',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/03/Dash-DAQ-_-Tektronix-Oscilloscope-Tektronic-Function-Generator-zoom@05x.jpg',
             description='''
             A function generator generates a variety of electrical waveforms.
@@ -673,7 +673,7 @@ layout = html.Div(className='gallery', children=[
             app_name='Pfeiffer Vacuum Gauge',
             app_link='https://www.dashdaq.io/read-pfeiffer-vacuum-gauge-pressure-in-python',
             code_name_display_text='Try the app',
-            code_link='https://dash-daq-pressure-gauge-pv.herokuapp.com/',
+            code_link='https://dash-gallery.plotly.host/dash-daq-pressure-gauge-pv/',
             img_src='https://www.dashdaq.io/wp-content/uploads/2018/03/Dash-DAQ-_-Pfeiffer@05x.jpg',
             description='''
             In just over 300 lines of code, this app helps you control
