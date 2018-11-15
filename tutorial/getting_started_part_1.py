b2d33f3685731647e290a616f9999a3a940b1baf
Philippe Duval <philippe@plot.ly>
2018-11-15 10:48:57 -0500

    Add Making your first change with hot-reload.

--- tutorial/getting_started_part_1.py
+++ tutorial/getting_started_part_1.py
@@ -100,6 +100,17 @@ layout = html.Div([
            app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
            ```
            to get the same look and feel of these examples.
+           
+    ### Making your first change
+
+    Dash includes "hot-reloading", This features is activated by default when
+    you run your app with `app.run_server(debug=True). This means that Dash
+    will automatically refresh your browser when you make a change in your code.
+
+    Give it a try: change the title "Hello Dash" in your application or change the `x` or the `y` data. Your app should auto-refresh with your change.
+
+    > Don't like hot-reloading? You can turn this off with `app.run_server(dev_tools_hot_reload=False)`. 
+    > Learn more in [Dash Dev Tools documentation](...). Questions? See the [community forum hot reloading discussion](https://community.plot.ly/t/announcing-hot-reload/14177).
 
     #### More about HTML
 
