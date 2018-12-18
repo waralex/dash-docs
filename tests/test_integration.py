9d6de23dd6b26d1772c9d00f3b42043413bda35e
Marc-André Rivet <marcandre.rivet@sociablelabs.com>
2018-12-18 17:54:51 -0500

    revert test changes

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -64,7 +64,12 @@ class Tests(IntegrationTests):
     def test_docs(self):
         self.startServer(app, '/')
 
-        time.sleep(5)
+        try:
+            self.wait_for_element_by_id('wait-for-layout')
+        except Exception as e:
+            print(self.wait_for_element_by_id(
+                '_dash-app-content').get_attribute('innerHTML'))
+            raise e
 
         self.snapshot('index - 1')
 
@@ -111,6 +116,7 @@ class Tests(IntegrationTests):
 
         def visit_and_snapshot(href):
             self.driver.get('http://localhost:8050{}'.format(href))
+            self.wait_for_element_by_id('wait-for-page-{}'.format(href))
             time.sleep(5)
             self.snapshot(href)
             self.driver.back()
