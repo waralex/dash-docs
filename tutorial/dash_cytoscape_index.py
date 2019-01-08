becc53475927399310524535cac92d53f8b58274
Xing Han Lu <xhlperso@gmail.com>
2019-01-07 21:30:27 -0500

    Add Chapters to Cytoscape Homepage

--- tutorial/dash_cytoscape_index.py
+++ tutorial/dash_cytoscape_index.py
@@ -84,6 +84,68 @@ layout = html.Div([
             '''
         ),
 
+        Chapter(
+            'Part 3. Styling',
+            '/cytoscape/styling',
+            '''
+            Modify the color, shape and style of your elements with a syntax
+            similar to CSS. Cytoscape incldues a wide variety of properties,
+            equiping you with everything you need to display your graphs with
+            aesthetics, creativity, and understandability in mind. This chapter
+            covers:
+            - Basic style properties for nodes and edges
+            - Using selectors to modify specific groups of elements
+            - Organize your edges with curve and line properties
+            - Advanced node styling with pie charts and images
+            '''
+        ),
+
+        Chapter(
+            'Part 4. Dash Callbacks',
+            '/cytoscape/callbacks',
+            '''
+            Update your layout, elements, or style with other Dash components
+            using callbacks. This chapter covers:
+            - Adding and removing elements
+            - Modifying the layout and the stylesheet
+            - Advanced usage of callbacks
+            '''
+        ),
+
+        Chapter(
+            'Part 5. Events Callbacks for User Interactions',
+            '/cytoscape/events',
+            '''
+            Dash Cytoscape fires callbacks whenever the user interact with the
+            graph you created, which can be used to update the graph itself,
+            or to interact with other components. This chapter includes 
+            examples for:
+            - Simple integrations with HTML components
+            - Different types of data returned
+            - Node versus edges callbacks
+            - Tap, hover or select callbacks
+            '''
+        ),
+
+        Chapter(
+            'Part 6. Applications',
+            '/cytoscape/applications',
+            '''
+            We show how Dash Cytoscape can be applied for fields as diverse as
+            bioinformatics and social networks analysis. Examples include:
+            - Displaying Phylogeny Data with Biopython
+            - Interactive exploration of large social networks
+            '''
+        ),
+
+
+        Chapter(
+            'Part 7. Object Reference',
+            '/cytoscape/reference',
+            '''
+            You can find here all the properties of the `Cytoscape` object.
+            '''
+        )
     ]),
 
     Section('Roadmap, Sponsorships, and Contact', dcc.Markdown(dedent(
