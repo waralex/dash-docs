2eee54db32543cdd3250b4aaaef6dbdc7d0e5887
Xing Han Lu <xhlperso@gmail.com>
2019-01-09 22:50:21 -0500

    Finish Draft for Styling Chapter

--- tutorial/cytoscape/styling_chapter.py
+++ tutorial/cytoscape/styling_chapter.py
@@ -3,37 +3,31 @@ from textwrap import dedent
 import dash_cytoscape
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
 
-from .utils import section_title, CreateDisplay
+from .utils import CreateDisplay
 from tutorial import tools, styles
 
-'''
-- Using selectors to modify specific groups of elements
-- Organize your edges with curve and line properties
-- Advanced node styling with pie charts and images
-'''
 
-# examples = {
-#     example: tools.load_example('tutorial/examples/table/{}'.format(example))
-#     for example in ['dropdown_per_column.py', 'dropdown_per_row.py']
-# }
+examples = {
+    example: tools.load_example('tutorial/examples/cytoscape/{}'.format(example))
+    for example in ['images.py']
+}
 
 simple_elements = [
     {
         'data': {'id': 'one', 'label': 'Modified Color'},
         'position': {'x': 75, 'y': 75},
-        'classes': 'red' # Single class
+        'classes': 'red'  # Single class
     },
     {
         'data': {'id': 'two', 'label': 'Modified Shape'},
         'position': {'x': 75, 'y': 200},
-        'classes': 'triangle' # Single class
+        'classes': 'triangle'  # Single class
     },
     {
         'data': {'id': 'three', 'label': 'Both Modified'},
         'position': {'x': 200, 'y': 75},
-        'classes': 'red triangle' # Multiple classes
+        'classes': 'red triangle'  # Multiple classes
     },
     {
         'data': {'id': 'four', 'label': 'Regular'},
@@ -60,11 +54,47 @@ weighted_elements = [
     {'data': {'source': 'D', 'target': 'A', 'weight': 6}}
 ]
 
+named_elements = [
+    {'data': {'id': 'A', 'firstname': 'Albert'}},
+    {'data': {'id': 'B', 'firstname': 'Bert'}},
+    {'data': {'id': 'C', 'firstname': 'Charlie'}},
+    {'data': {'id': 'D', 'firstname': 'Daniela'}},
+    {'data': {'id': 'E', 'firstname': 'Emma'}},
+
+    {'data': {'source': 'A', 'target': 'B', 'weight': 1}},
+    {'data': {'source': 'A', 'target': 'C', 'weight': 2}},
+    {'data': {'source': 'B', 'target': 'D', 'weight': 3}},
+    {'data': {'source': 'B', 'target': 'E', 'weight': 4}},
+    {'data': {'source': 'C', 'target': 'E', 'weight': 5}},
+    {'data': {'source': 'D', 'target': 'A', 'weight': 6}}
+]
+
+
+double_edges = [
+    {'data': {'id': src+tgt, 'source': src, 'target': tgt}}
+    for src, tgt in ['AB', 'BA', 'BC', 'CB', 'CD', 'DC', 'DA', 'AD']
+]
+
+double_edged_el = [{'data': {'id': id_}} for id_ in 'ABCD'] + double_edges
+
+
+directed_edges = [
+    {'data': {'id': src+tgt, 'source': src, 'target': tgt}}
+    for src, tgt in ['BA', 'BC', 'CD', 'DA']
+]
+
+directed_elements = [{'data': {'id': id_}} for id_ in 'ABCD'] + directed_edges
+
+
 Display = CreateDisplay({
     'dash_cytoscape': dash_cytoscape,
     'simple_elements': simple_elements,
-    'weighted_elements': weighted_elements
+    'weighted_elements': weighted_elements,
+    'named_elements': named_elements,
+    'double_edged_el': double_edged_el,
+    'directed_elements': directed_elements
 })
+
 
 layout = html.Div([
 
@@ -173,30 +203,30 @@ layout = html.Div([
     elements. Notice in the example above that if an element has two or more
     classes, it will accept all the styles that select it.
     
-    Here, we modify the background color and line color of all the elements of
-    class "red". This means that if the element is a node, then it will be 
-    filled with red, and it is an edge, then the color of the line will be red.
-    Although this offers great flexibility, you need to be careful with your 
-    declaration, since you won't receive warning if you use the wrong key or
-    make a typo. Standard RGB and Hex colors are accepted, along with basic 
-    colors recognized by CSS.
+    Here, we are simultaneously modifying the background and line color of all 
+    the elements of class "red". This means that if the element is a node, then 
+    it will be filled with red, and it is an edge, then the color of the line 
+    will be red. Although this offers great flexibility, you need to be careful 
+    with your declaration, since you won't receive warning if you use the wrong 
+    key or make a typo. Standard RGB and Hex colors are accepted, along with 
+    basic colors recognized by CSS.
     
     Additionally, the `content` key takes as value what you want to display 
     above or next to the element on the screen, which in this case is the 
     label inside the `data` dictionary of the input element. Since we defined 
     a label for each element, that label will be displayed for every node.
     
-    ## Comparing data items
+    ## Comparing data items using selectors
     
-    A nice property of the selector is that you select elements by comparing
-    the value of an item in each of the data dictionaries. Say we have some
-    nodes with IDs A to F. One of the edges might be declared the following 
-    way:
+    A nice property of the selector is that it can select elements by comparing
+    a certain item of the data dictionaries with a given value. Say we have 
+    some nodes with `id` A to E declared this way:
     ```
     {'data': {'source': 'A', 'target': 'B', 'weight': 1}}
     ```
-    The `'weight'` key indicates the weight of your edge. You can find the 
-    declaration below:
+    where the `'weight'` key indicates the weight of your edge. You can find 
+    the declaration below:
+    
     ''')),
 
     html.Details(open=False, children=[
@@ -216,12 +246,12 @@ layout = html.Div([
             {'data': {'source': 'C', 'target': 'E', 'weight': 5}},
             {'data': {'source': 'D', 'target': 'A', 'weight': 6}}
         ]
-    '''))
+        '''))
     ]),
 
     dcc.Markdown(dedent('''
     If you want to highlight all the of the edges above a certain weight 
-    (e.g. 3), simply use the selector `'[weight>3]'`. Here's an example:
+    (e.g. 3), use the selector `'[weight > 3]'`. For example:
     ''')),
 
     Display('''
@@ -234,7 +264,7 @@ layout = html.Div([
             {
                 'selector': 'edge',
                 'style': {
-                    'content': 'data(weight)'
+                    'label': 'data(weight)'
                 }
             },
             {
@@ -245,6 +275,350 @@ layout = html.Div([
             }
         ]
     )
-    ''')
+    '''),
 
+    dcc.Markdown(dedent('''
+    Similarly, if you want to have weights smaller or equal to 3, you would
+    write:
+    ''')),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=weighted_elements,
+        stylesheet=[
+            {
+                'selector': 'edge',
+                'style': {
+                    'label': 'data(weight)'
+                }
+            },
+            {
+                'selector': '[weight <= 3]',
+                'style': {
+                    'line-color': 'blue'
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    Comparisons also work for string matching problems. Given the same graph
+    as before, but with a data key `'firstname'` for each node:
+    ```
+    {'data': {'id': 'A', 'firstname': 'Albert'}}
+    ```
+    We can select all the elements that match a specific pattern. For instance,
+    to style nodes where `'firstname'` contains the substring `'ert'`, we 
+    declare:
+    ''')),
+
+    html.Details(open=False, children=[
+        html.Summary('View named elements'),
+        dcc.SyntaxHighlighter(dedent('''
+        named_elements = [
+            {'data': {'id': 'A', 'firstname': 'Albert'}},
+            {'data': {'id': 'B', 'firstname': 'Bert'}},
+            {'data': {'id': 'C', 'firstname': 'Charlie'}},
+            {'data': {'id': 'D', 'firstname': 'Daniela'}},
+            {'data': {'id': 'E', 'firstname': 'Emma'}},
+    
+            {'data': {'source': 'A', 'target': 'B'}},
+            {'data': {'source': 'A', 'target': 'C'}},
+            {'data': {'source': 'B', 'target': 'D'}},
+            {'data': {'source': 'B', 'target': 'E'}},
+            {'data': {'source': 'C', 'target': 'E'}},
+            {'data': {'source': 'D', 'target': 'A'}}
+        ]
+        '''))
+    ]),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=named_elements,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(firstname)'
+                }
+            },
+            {
+                'selector': '[firstname *= "ert"]',
+                'style': {
+                    'background-color': '#FF4136',
+                    'shape': 'rectangle'
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    Now, if we want to select all the elements where `'firstname'` *does not*
+    contain `'ert'`, then we can run:
+    ''')),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=named_elements,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(firstname)'
+                }
+            },
+            {
+                'selector': '[firstname !*= "ert"]',
+                'style': {
+                    'background-color': '#FF4136',
+                    'shape': 'rectangle'
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    Other options also exist for matching specific parts of the string. For 
+    example, if we want to only select the prefix, we can use `^=` as such:
+    ''')),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=named_elements,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(firstname)'
+                }
+            },
+            {
+                'selector': '[firstname ^= "Alb"]',
+                'style': {
+                    'background-color': '#FF4136',
+                    'shape': 'rectangle'
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    This can also be prepended by modifiers. For example, `@` added in front
+    of an operator will render the string matched case insensitive.
+    ''')),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=named_elements,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(firstname)'
+                }
+            },
+            {
+                'selector': '[firstname @^= "alb"]',
+                'style': {
+                    'background-color': '#FF4136',
+                    'shape': 'rectangle'
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    The complete list of matching operations for `data` dictionaries can be 
+    found [here](http://js.cytoscape.org/#selectors/data).
+    
+    ## Styling edges
+    
+    ### Two-sided edges and curvature
+    
+    Many methods exist to style edges in Dash Cytoscape. In some cases, you
+    might want different ways to display double-edged 
+    ''')),
+
+    html.Details(open=False, children=[
+        html.Summary('View double-edged elements'),
+        dcc.SyntaxHighlighter(dedent('''
+        double_edges = [
+            {'data': {'id': src+tgt, 'source': src, 'target': tgt}}
+            for src, tgt in ['AB', 'BA', 'BC', 'CB', 'CD', 'DC', 'DA', 'AD']
+        ]
+    
+        double_edged_el = [{'data': {'id': id_}} for id_ in 'ABCD'] + double_edges
+        '''))
+    ]),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=double_edged_el,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(id)'
+                }
+            },
+            {
+                'selector': '#AB, #BA',
+                'style': {
+                    'curve-style': 'bezier',
+                    'label': 'bezier',
+                    'line-color': 'red'
+                }
+            },
+            {
+                'selector': '#AD, #DA',
+                'style': {
+                    'curve-style': 'haystack',
+                    'label': 'haystack',
+                    'line-color': 'blue'
+                }
+            },
+            {
+                'selector': '#DC, #CD',
+                'style': {
+                    'curve-style': 'segments',
+                    'label': 'segments',
+                    'line-color': 'green'
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    Many curve styles are accepted, and support further customization such as 
+    the distance between edges and curvature radius. You can find them in 
+    the [JavaScript docs](http://js.cytoscape.org/#style/bezier-edges).
+    
+    ### Edge Arrows
+    
+    To better highlight the directed edges, we can add arrows of different 
+    shapes, colors, and positions onto the edges. This is an example of using
+    different types of arrows, with the same graph above, but with certain
+    edges removed:
+    ''')),
+
+    html.Details(open=False, children=[
+        html.Summary('View directed elements'),
+        dcc.SyntaxHighlighter(dedent('''
+        directed_edges = [
+            {'data': {'id': src+tgt, 'source': src, 'target': tgt}}
+            for src, tgt in ['BA', 'BC', 'CD', 'DA']
+        ]
+        
+        directed_elements = [{'data': {'id': id_}} for id_ in 'ABCD'] + directed_edges
+        '''))
+    ]),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        layout={'name': 'circle'},
+        style={'width': '100%', 'height': '400px'},
+        elements=directed_elements,
+        stylesheet=[
+            {
+                'selector': 'node',
+                'style': {
+                    'label': 'data(id)'
+                }
+            },
+            {
+                'selector': 'edge',
+                'style': {
+                    # The default curve style does not work with certain arrows
+                    'curve-style': 'bezier'
+                }
+            },
+            {
+                'selector': '#BA',
+                'style': {
+                    'source-arrow-color': 'red',
+                    'source-arrow-shape': 'triangle',
+                    'line-color': 'red'
+                }
+            },
+            {
+                'selector': '#DA',
+                'style': {
+                    'target-arrow-color': 'blue',
+                    'target-arrow-shape': 'vee',
+                    'line-color': 'blue'
+                }
+            },
+            {
+                'selector': '#BC',
+                'style': {
+                    'mid-source-arrow-color': 'green',
+                    'mid-source-arrow-shape': 'diamond',
+                    'mid-source-arrow-fill': 'hollow',
+                    'line-color': 'green',
+                }
+            },
+            {
+                'selector': '#CD',
+                'style': {
+                    'mid-target-arrow-color': 'black',
+                    'mid-target-arrow-shape': 'circle',
+                    'arrow-scale': 2,
+                    'line-color': 'black',
+                }
+            }
+        ]
+    )
+    '''),
+
+    dcc.Markdown(dedent('''
+    Notice here that we prepend a position indicator for the color and shape
+    keys. In the previous example, all four possible positions are displayed.
+    In fact, you can even the edges with multiple arrows, all with different
+    colors and shapes. You can find all the possible configurations for [edge
+    arrows here](http://js.cytoscape.org/#style/edge-arrow).
+    
+    
+    ## Displaying Images
+    
+    It is possible to display images inside nodes, as 
+    [documented in Cytoscape.js](http://js.cytoscape.org/#style/background-image).
+    We show below a complete example of display an interactive tree of animal
+    phylogeny using images from Wikimedia.
+    ''')),
+
+    dcc.SyntaxHighlighter(
+        examples['images.py'][0],
+        language='python',
+        customStyle=styles.code_container
+    ),
+
+    html.Div(
+        examples['images.py'][1],
+        className='example-images'
+    )
 ])
