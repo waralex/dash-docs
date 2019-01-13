748e68ab17adbbc82565844d94ec6d9ca710b8f5
Xing Han Lu <xhlperso@gmail.com>
2019-01-12 19:24:27 -0500

    Update section on changing layouts with callbacks

--- tutorial/cytoscape/callbacks_chapter.py
+++ tutorial/cytoscape/callbacks_chapter.py
@@ -11,7 +11,7 @@ from tutorial import tools, styles
 
 examples = {
     example: tools.load_example('tutorial/examples/cytoscape/{}'.format(example))
-    for example in ['update_layout.py']
+    for example in ['update_layout.py', 'stylesheet_callbacks.py']
 }
 
 
@@ -55,10 +55,29 @@ edges = [
     )
 ]
 
+default_stylesheet = [
+    {
+        'selector': 'node',
+        'style': {
+            'background-color': 'BFD7B5',
+            'label': 'data(label)'
+        }
+    },
+    {
+        'selector': 'edge',
+        'style': {
+            'line-color': '#A3C4BC'
+        }
+    }
+]
+
+
 Display = CreateDisplay({
     'dash_cytoscape': dash_cytoscape,
     'elements': nodes + edges,
-    'dcc': dcc
+    'html': html,
+    'dcc': dcc,
+    'default_stylesheet': default_stylesheet
 })
 
 layout = html.Div([
@@ -77,7 +96,7 @@ layout = html.Div([
     you are familiar with them; this chapter will go over Dash callbacks 
     specific to Dash Cytoscape.
     
-    ## Layouts
+    ## Changing Layouts
     
     Consider the graph containing North American cities from the layout 
     chapter. We have shown in that chapter how to display the same graph in
@@ -193,6 +212,138 @@ layout = html.Div([
     html.Div(
         examples['update_layout.py'][1],
         className='example-container'
+    ),
+
+    dcc.Markdown(dedent('''
+    ## Interactively update styles
+    
+    Updating the stylesheet using Dash components is similar to updating
+    layouts, although it can get more complex. Indeed, you can choose to create
+    a default stylesheet, and append new styles to that stylesheet every time
+    a designated callback is fired. Let's take the following stylesheet:
+    
+    ```python
+    default_stylesheet = [
+        {
+            'selector': 'node',
+            'style': {
+                'background-color': 'BFD7B5',
+                'label': 'data(label)'
+            }
+        },
+        {
+            'selector': 'edge',
+            'style': {
+                'line-color': '#A3C4BC'
+            }
+        }
+    ]
+    ```
+    
+    This is generally declared at the beginning of your script, before layout
+    declaration (therefore it is shared accross sessions). The city graph will 
+    look something like this:
+    ''')),
+
+    Display('''
+    dash_cytoscape.Cytoscape(
+        id='cytoscape',
+        elements=elements,
+        stylesheet=default_stylesheet,
+        style={'width': '100%', 'height': '400px'},
+        layout={
+            'name': 'circle'
+        }
     )
+    '''),
+
+    dcc.Markdown(dedent('''
+    We might want to use text fields to input the color we want to add: 
+    ''')),
+
+    Display('''
+    html.Div([
+        html.Div(style={'width': '50%', 'display': 'inline'}, children=[
+            'Edge Color:',
+            dcc.Input(id='input-line-color', type='text')
+        ]),
+        html.Div(style={'width': '50%', 'display': 'inline'}, children=[
+            'Node Color:',
+            dcc.Input(id='input-bg-color', type='text')
+        ])
+    ])
+    '''),
+
+    dcc.Markdown(dedent('''
+    All we need now is to assign a callback that will add new styles to the 
+    default stylesheet in order to change the default color:
+    
+    ```python
+    @app.callback(Output('cytoscape', 'stylesheet'),
+              [Input('input-line-color', 'value'),
+               Input('input-bg-color', 'value')])
+    def update_stylesheet(line_color, bg_color):
+        if line_color is None:
+            line_color = ''
+    
+        if bg_color is None:
+            bg_color = ''
+    
+        new_styles = [
+            {
+                'selector': 'node',
+                'style': {
+                    'background-color': bg_color
+                }
+            },
+            {
+                'selector': 'edge',
+                'style': {
+                    'line-color': line_color
+                }
+            }
+        ]
+    
+        return default_stylesheet + new_styles
+    ```
+    
+    Notice that we are setting the line and background color to an empty
+    string when they are set to `None`; this is to avoid feeding `None`
+    to the dictionary, since it is not accepted by `Cytoscape`. 
+    
+    However, passing any string value to the dictionary is accepted, even when 
+    the string value is not recognized. Therefore, the callback is fired every
+    time you type a new character, but the changes are only applied when
+    `Cytoscape` recognizes the input, which in this case could be the name of
+    a color, the hex code, or the rgb function.
+    
+    Below, we show how the entire app is constructed:
+    ''')),
+
+    dcc.SyntaxHighlighter(
+        examples['stylesheet_callbacks.py'][0],
+        language='python',
+        customStyle=styles.code_container
+    ),
+
+    html.Div(
+        examples['stylesheet_callbacks.py'][1],
+        className='example-container'
+    ),
+
+    dcc.Markdown(dedent('''
+    In this example, we are not appending the new styles
+    directly to the default style, but instead concatenating 
+    `default_stylesheet` with `new_styles`. This is because any modification
+    to `default_stylesheet` will be permanent, which is not a good thing if you
+    are hosting your app for many users (since `default_stylesheet` is shared
+    across all user sessions).
+    
+    If you want to find more examples of styling using callbacks,
+    check out `usage-stylesheet.py`, which presents a comprehensive overview
+    of techniques for manipulating stylesheets in Dash Cytoscape.
+    
+    ## Adding and removing elements
+    '''))
 
 ])
