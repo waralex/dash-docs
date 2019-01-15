498d146e975bf4b9cdd136900cac46d2740a89e3
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-01-15 15:10:43 -0500

    Remove meta tags to remove SEO.

--- run.py
+++ run.py
@@ -122,38 +122,6 @@ def display_content(pathname):
 
     return content
 
-app.index_string = '''
-<!DOCTYPE html>
-<html>
-    <head>
-        {%metas%}
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta
-            name="description"
-            content="Dash User Guide and Documentation. Dash is a Python framework for building analytical web apps in Python."
-        >
-        <title>{%title%}</title>
-        {%favicon%}
-        {%css%}
-        <!-- Google Tag Manager Tag -->
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-N6T2RXG');</script>
-    </head>
-    <body>
-        <!-- Google Tag Manager Tag -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6T2RXG"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        {%app_entry%}
-        <footer>
-            {%config%}
-            {%scripts%}
-        </footer>
-    </body>
-</html>
-'''
 
 if __name__ == '__main__':
     app.run_server(debug=True, threaded=True, port=8060)
