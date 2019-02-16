6153ea600ef25f5fa531bf0cbc775c0ffbe64efe
Ned Letcher <nletcher@gmail.com>
2019-02-16 22:27:19 +1100

    improve content

--- tutorial/integrating_dash.py
+++ tutorial/integrating_dash.py
@@ -28,7 +28,7 @@ layout = html.Div([
     The simplest approach to embedding Dash in an existing web application is to
     an include an `<iframe>` element in your HTML whose `src` attribute points
     towards the address of an already running Dash instance. This allows you to
-    place your Dash app in a specific location within an existing web page, with
+    place your Dash app in a specific location within an existing web page with
     your desired dimensions:""")),
     Syntax('<iframe src="http://localhost:8050" width=700 height=600>'),
     dcc.Markdown(dedent(
@@ -36,9 +36,10 @@ layout = html.Div([
     ## Embedding a Dash app within an Existing Flask App 
     
     As discussed in the [Deployment Chapter](/deployment), Dash uses the Flask
-    web framework under the hood. It is possible to take advantage of this and
-    embed a Dash app at a specific route of an existing Flask app. In the
-    following example, a Dash app is mounted at the `/dash` route (eg
+    web framework under the hood. This makes it fairly straightforward to 
+    embed a Dash app at a specific route of an existing Flask app.
+
+    In the following example, a Dash app is mounted at the `/dash` route (eg
     `http://localhost:8050/dash`) of a Flask app:
     """
     )),
@@ -68,7 +69,7 @@ layout = html.Div([
     )),
     dcc.Markdown(dedent(
     """
-    Note that it is important to set the `name` parameter of the Dash instance
+    > **Note**: it is important to set the `name` parameter of the Dash instance
     to the value `__name__`, so that Dash can correctly detect the location of
     any static assets inside an `assets` directory for this Dash app.
     """
@@ -80,16 +81,13 @@ layout = html.Div([
 
     This approach uses Werkzeug's
     [`DispatcherMiddleware`](http://werkzeug.pocoo.org/docs/latest/middlewares/)
-    to combine one or more Dash apps with one more existing WSGI apps. With this
-    strategy, your existing web apps don't have to be built on Flask, but
-    instead can be any Python web application implementing the [WSGI
-    specification](https://wsgi.readthedocs.io).
+    to combine one or more Dash apps with existing WSGI apps (including
+    Flask). It is useful when you want to combine multiple Dash apps or when
+    your existing app is a non-Flask WSGI app.
 
-    The following example illustrates this approach. In this example, the
-    existing app being combined with two Dash apps is a Flask app, however this
-    approach enables the combination of [any WSGI
-    app](https://wsgi.readthedocs.io/en/latest/frameworks.html) with multiple
-    Dash apps (or any other WSGI apps for that matter)."""
+    The following example illustrates this approach by combining two Dash apps
+    with a Flask app.
+    """
     )),
     dcc.Markdown("`flask_app.py`"),
     Syntax(dedent(
@@ -142,9 +140,6 @@ layout = html.Div([
     from app1 import app as app1
     from app2 import app as app2
 
-    app1.enable_dev_tools(debug=True)
-    app2.enable_dev_tools(debug=True)
-    
     application = DispatcherMiddleware(flask_app, {
         '/app1': app1.server,
         '/app2': app2.server,    
@@ -158,25 +153,17 @@ layout = html.Div([
     in a Flask server to the Dash apps, but let them create their own, which the
     `DispatcherMiddleware` routes requests to based on the prefix of the
     incoming requests. Within each Dash app, `requests_pathname_prefix` must be
-    provided with the app's mount point, as the mount points specified in the
-    `DispatcherMiddleware` initialization has effectively already set
-    `requests_pathname_prefix` with the correct value.
-
-    In order to enable the development tools, the `enable_dev_tools` method
-    needs to be invoked manually for each Dash instance. This is because when
-    running using a WSGI server, Dash's `run_server` method (which calls
-    `enable_dev_tools`) is not used, as well as the fact that there are two
-    distinct Dash instances.
+    specified as the app's mount point, in order to match the route prefix
+    set by the `DispatcherMiddleware`.
 
     Note that the `application` object in `wsgi.py` is of type
     `werkzeug.wsgi.DispatcherMiddleware`, which does not have a `run`
-    method. This can be run as a WSGI app like so (the `--reload` flag is
-    required for hot reloading to work):
-
-    ```
-    $ gunicorn --reload wsgi:application
-    ```
-    
+    method. This can be run as a WSGI app like so:
+    """
+    )),
+    Syntax("$ gunicorn wsgi:application"),
+    dcc.Markdown(dedent(
+    """
     Alternatively, you can use the Werkzeug development server (which is not
     suitable for production) to run the app:
 
@@ -191,9 +178,6 @@ layout = html.Div([
     from app1 import app as app1
     from app2 import app as app2
 
-    app1.enable_dev_tools(debug=True)
-    app2.enable_dev_tools(debug=True)
-    
     application = DispatcherMiddleware(flask_app, {
         '/app1': app1.server,
         '/app2': app2.server,    
@@ -205,15 +189,31 @@ layout = html.Div([
     )),
     dcc.Markdown(dedent(
     """
-    You would run the above with:
+    If you need access to the Dash development tools when using this approach
+    (whether running with a WSGI server, or using the Werkzeug development
+    server) you must invoke them manually for each Dash app. The following lines
+    can be added before the initialisation of the `DispatcherMiddleware` to do this:
+    """
+    )),
+    Syntax(dedent(
+    """
+    app1.enable_dev_tools(debug=True)
+    app2.enable_dev_tools(debug=True)
+    """
+    )),
+    dcc.Markdown(dedent(
+    """    
+    > **Note:** debug mode should not be enabled in production. When using debug
+        mode with Gunicorn, the `--reload` command line flag is required for hot
+        reloading to work.
 
-    ```
-    $ python run.py
-    ```
-
-    > **Note:** debug mode should not be enabled in production. When deploying
-    > the above examples to production, you should set `debug=False` and not use
-    > the `--reload` flag for Gunicorn.
+    In this example, the existing app being combined with two Dash apps is a
+    Flask app, however this approach enables the combination of any web
+    application implementing the [WSGI
+    specification](https://wsgi.readthedocs.io). A list of WSGI web frameworks
+    can be found in the [WSGI
+    documentation](https://wsgi.readthedocs.io/en/latest/frameworks.html) with
+    one or more Dash apps.
     """
     )),
     ])
