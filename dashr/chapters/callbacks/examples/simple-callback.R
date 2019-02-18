aa5ad5851f6b0f9a003e23e6206366542a8dd9c3
Chris P <chris@plot.ly>
2019-02-18 18:11:44 -0500

    :wave: skeleton for a callback chapter

---
+++ dashr/chapters/callbacks/examples/simple-callback.R
@@ -0,0 +1,20 @@
+library(dashR)
+library(dashCoreComponents)
+library(dashHtmlComponents)
+
+app <- Dash$new()
+
+app$layout_set(htmlDiv(list(
+    coreInput(id='my-id', value='initial value', type='text'),
+    htmlDiv(id='my-div')
+)))
+
+
+app$callback(
+    output=list(id='my-div', property='children'),
+    params=list(input(id='my-id', property='value')),
+    function(input_value) {
+  sprintf("You've entered \"%s\"", input_value)
+})
+
+app$run_server()
---
+++ dashr/chapters/callbacks/index.R
@@ -0,0 +1,29 @@
+library(dashCoreComponents)
+library(dashHtmlComponents)
+
+utils <- new.env()
+source('dashr/utils.R', local=utils)
+
+examples <- list(
+  simple.callbacks=utils$LoadExampleCode('dashR/chapters/callbacks/examples/simple-callback.R')
+)
+
+layout <- htmlDiv(list(
+  coreMarkdown("
+In the previous chapter on the app.layout we learned that the
+app$layout describes what the app looks like and is a
+hierarchical tree of components.
+The dash_html_components library provides classes for
+all of the HTML tags, and the keyword arguments describe
+the HTML attributes like style, className, and id.
+The dash_core_components library generates higher-level components
+like controls and graphs.
+
+This chapter describes how to make your Dash apps
+interactive.
+
+Let's get started with a simple example.
+  "),
+  examples$simple.callbacks$source_code,
+  examples$simple.callbacks$layout
+))
