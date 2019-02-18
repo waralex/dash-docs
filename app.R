67b6afb1e538dfc83fa5e33357b39ded7d77d53c
Chris P <chris@plot.ly>
2019-02-18 18:11:10 -0500

    :wrench: util to eval standalone examples

---
+++ app.R
@@ -0,0 +1,3 @@
+library(dashR)
+
+app <- Dash$new()
---
+++ dashr/utils.R
@@ -0,0 +1,42 @@
+library(dashCoreComponents)
+source('app.R')
+
+LoadExampleCode <- function(filename) {
+  # Take a self-contained DashR example filename,
+  # eval it, and return that example's `layout`
+  # and the source code.
+  example.file.as.string <- readChar(filename, file.info(filename)$size);
+
+  # modify the example code so that it can run within
+  # the context of an already running app
+  example.ready.for.eval <- example.file.as.string
+  replacements = list(
+    # set the layout to a variable so that we can access it
+    # and return it
+    list('app\\$layout_set\\(', 'layout <- htmlDiv\\('),
+
+    # Since app is in the namespace from the `app.R` import,
+    # it will implicity be picked up by the
+    # `eval` call below
+    list('app <- Dash\\$new\\(\\)', ''),
+    list('app\\$run_server\\(\\)', '')
+  )
+  for(replacement in replacements) {
+    example.ready.for.eval <- gsub(
+      replacement[1],
+      replacement[2],
+      example.ready.for.eval
+    )
+  }
+
+  # run the example and implicitly assign the `layout` variable
+  eval(parse(text=example.ready.for.eval))
+
+  return(list(
+    layout=htmlDiv(className='example-container', children=layout),
+    source_code=htmlDiv(
+      children=coreSyntaxHighlighter(example.file.as.string),
+      className='code-container'
+    )
+  ))
+}
