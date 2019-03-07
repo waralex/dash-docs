2569eb27c975b5b2e887e09a75a091bfdac47439
Byron Zhu <byron@plot.ly>
2019-03-07 16:35:36 -0500

    change back

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -135,5 +135,5 @@ class Tests(IntegrationTests):
         search_element.clear()
         search_element.send_keys('dropdown')
         self.wait_for_element_by_id('hits')
-        time.sleep(2)
+        time.sleep(1)
         self.snapshot('search-dropdown')
