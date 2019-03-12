07a42c9cef7532dfa2d730d54ba328dd7b05cdb7
Ryan Patrick Kyle <ryan@plot.ly>
2019-03-12 01:19:35 -0400

    testing if else in shell script

--- web.sh
+++ web.sh
@@ -1,1 +1,4 @@
-fakechroot fakeroot chroot /app/.root /bin/sh -c 'cd /app && /usr/bin/R -f /app/run.R --gui-none --no-save'
+if [ "$LANGUAGE" = "PYTHON" ]
+    gunicorn app:runserver
+else
+    fakechroot fakeroot chroot /app/.root /bin/sh -c 'cd /app && /usr/bin/R -f /app/run.R --gui-none --no-save'
