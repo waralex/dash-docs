01f8cc1e04f045cd97b8a319565835e036391b90
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-12 16:32:13 -0400

    [WIP] add chapt 7 structure to work this week

--- run.R
+++ run.R
@@ -15,6 +15,8 @@ chapters.graph_crossfiltering <- new.env()
 source('dashr/chapters/graph-crossfiltering/index.R', local=chapters.graph_crossfiltering)
 chapters.data_callbacks <- new.env()
 source('dashr/chapters/data-callbacks/index.R', local=chapters.data_callbacks)
+chapters.faqs_gotchas <- new.env()
+source('dashr/chapters/faqs-gotchas/index.R', local=chapters.faqs_gotchas)
 
 # Temporary workaround until `assets/` are loaded
 # This will serve the CSS files in `assets` from a separate
@@ -66,6 +68,8 @@ app$callback(output=list(id='chapter', property='children'),
     return(chapters.graph_crossfiltering$layout)
   } else if (pathname == "/data-callbacks") {
     return(chapters.data_callbacks$layout)
+  } else if (pathname == "/faqs-gotchas") {
+    return(chapters.faqs_gotchas$layout)
   } else {
     return(htmlDiv(list(
       htmlH1('DashR User Guide'),
@@ -97,6 +101,11 @@ app$callback(output=list(id='chapter', property='children'),
       dccLink(
         'Part 6. Sharing Data Between Callbacks',
         href='/data-callbacks'
+      ),
+      htmlBr(),
+      dccLink(
+        'Part 7. FAQs and Gotchas',
+        href='/faqs-gotchas'
       )
     )))
   }
