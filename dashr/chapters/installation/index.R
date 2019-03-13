722347445013714dfb3baf7d6411076bf908ca16
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-13 14:57:35 -0400

    :construction_worker: content started chap. 1

--- dashr/chapters/installation/index.R
+++ dashr/chapters/installation/index.R
@@ -3,25 +3,24 @@ library(dashHtmlComponents)
 library(dashR)
 
 layout <- htmlDiv(list(
-  htmlH1('Dash Installation'),
-  dccMarkdown("
-    > In your terminal, install several dash libraries. 
-    > These libraries are under active development, so install and upgrade frequently. 
-    > Make sure you're on at least version `3.0.2` of R. 
-    > You can see what version of R you have by entering version in the R CLI. 
-    > CRAN is the easiest place to download the latest R version.
+htmlH1('Dash Installation'),
+dccMarkdown("
+> In your terminal, install several dash libraries. 
+> These libraries are under active development, so install and upgrade frequently. 
+> Make sure you're on at least version `3.0.2` of R. 
+> You can see what version of R you have by entering version in the R CLI. 
+> CRAN is the easiest place to download the latest R version.
+"),
   
-  "),
-  dccMarkdown("
+dccMarkdown("
+```
+library(devtools) # devtools: Tools to Make Developing R Packages Easier  
+install_github('plotly/dashR') # The core dash backend
+install_github('plotly/dash-html-components') # HTML components   
+install_github('plotly/dash-dcc-components') # Supercharged components
+```
 
-    ```
-    library(devtools) # devtools: Tools to Make Developing R Packages Easier  
-    install_github('plotly/dashR') # The core dash backend
-    install_github('plotly/dash-html-components') # HTML components   
-    install_github('plotly/dash-dcc-components') # Supercharged components
-    ```
-
-    Ready? Now, let's [make your first Dash app](/getting-started).
-  ")
+Ready? Now, let's [make your first Dash app](/getting-started).
+")
   
 ))
