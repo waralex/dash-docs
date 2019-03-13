034b2488f67eb2ffc444233c43dfee218d50abb8
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-13 15:46:42 -0400

    :construction_worker: content chap. 1 :white_check_mark:

--- dashr/chapters/installation/index.R
+++ dashr/chapters/installation/index.R
@@ -30,15 +30,32 @@ dccMarkdown("
 > These docs are run using the versions listed above and 
 > these versions should be the latest versions available. 
 > To check which version that you have installed, you can run e.g.
-
+> 
 > ```{r}
 > library(dashCoreComponents) 
 > packageVersion('dashCoreComponents')
 > ```
 > 
+> 
 > To see the latest changes of any package, check the GitHub repo's CHANGELOG.md file:
 > - [dash changelog](https://github.com/plotly/dash/blob/master/CHANGELOG.md)
-
+> - [dash-core-components changelog](https://github.com/plotly/dash-core-components/blob/master/CHANGELOG.md)
+> - [dash-html-components changelog](https://github.com/plotly/dash-html-components/blob/master/CHANGELOG.md)
+> - [dash-table changelog](https://github.com/plotly/dash-table/blob/master/CHANGELOG.md)
+> 
+> Finally, note that the plotly package and the dash-renderer package are 
+> important package dependencies that are installed automatically 
+> with dash-core-components and dash respectively. 
+> These docs are using dash-renderer==0.20.0 and plotly==3.3.0 and 
+> their changelogs are located here:
+> 
+> - [dash-renderer changelog](https://github.com/plotly/dash-renderer/blob/master/CHANGELOG.md)
+> - [plotly changelog](https://github.com/plotly/plotly.py/blob/master/CHANGELOG.md)
+> All of these packages adhere to [semver](https://semver.org/).
+"),
+htmlHr(),
+dccMarkdown("
+[Back to the Table of Contents](/)
 ")
   
 ))
