e4c1f9e8f188249cf9942fd859c74ad48cbf06ab
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-14 00:05:33 -0400

    :construction_worker: content chap. 2

--- dashr/chapters/getting-started/index.R
+++ dashr/chapters/getting-started/index.R
@@ -67,47 +67,7 @@ dccMarkdown("
   examples$hello.world$layout,
 
 dcc.Markdown("
-Note:
-1. The `layout` is composed of a tree of \"components\" like `html.Div`
-and `dcc.Graph`.
-2. The `dash_html_components` library has a component for every HTML
-tag. The `html.H1(children='Hello Dash')` component generates
-a `<h1>Hello Dash</h1>` HTML element in your application.
-3. Not all components are pure HTML. The `dash_core_components` describe
-higher-level components that are interactive and are generated with
-JavaScript, HTML, and CSS through the React.js library.
-4. Each component is described entirely through keyword attributes.
-Dash is _declarative_: you will primarily describe your application
-through these attributes.
-5. The `children` property is special. By convention, it's always the
-first attribute which means that you can omit it:
- `html.H1(children='Hello Dash')` is the same as `html.H1('Hello Dash')`.
-Also, it can contain a string, a number, a single component, or a
-list of components.
-6. The fonts in your application will look a little bit different than
-what is displayed here. This application is using a
-custom CSS stylesheet to modify the default styles of the elements.
-You can learn more in the [css tutorial](/external-resources),
-but for now you can initialize your app with
-```
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-```
-to get the same look and feel of these examples.
-
-### Making your first change
-
-**New in dash 0.30.0 and dash-renderer 0.15.0**
- Dash includes \"hot-reloading\", this features is activated by default when
-you run your app with `app.run_server(debug=True)`. This means that Dash
-will automatically refresh your browser when you make a change in your code.
-Give it a try: change the title \"Hello Dash\" in your application or change the `x` or the `y` data. Your app should auto-refresh with your change.
-> Don't like hot-reloading? You can turn this off with `app.run_server(dev_tools_hot_reload=False)`. 
-> Learn more in [Dash Dev Tools documentation](/devtools) Questions? See the [community forum hot reloading discussion](https://community.plot.ly/t/announcing-hot-reload/14177).
-#### More about HTML
-The `dash_html_components` library contains a component class for every
-HTML tag as well as keyword arguments for all of the HTML arguments.
 "),
 
 
