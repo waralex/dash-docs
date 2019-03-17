f351cbae1e5d0b42d9085440914b0074e4ed42cd
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-17 19:09:30 -0400

    [WIP] header v1

--- init.R
+++ init.R
@@ -1,7 +1,7 @@
 # init.R
 #
 
-install.packages(c("devtools", "fiery", "routr", "base64enc"))
+# install.packages(c("devtools", "fiery", "routr", "base64enc"))
 #install.packages("/app/localpkgs/dashHtmlComponents_0.13.5.tar.gz", repos=NULL, type="source")
 #install.packages("/app/localpkgs/dashCoreComponents_0.43.0.tar.gz", repos=NULL, type="source")
 #install.packages("/app/localpkgs/dashR_0.0.2.tar.gz", repos=NULL, type="source")
