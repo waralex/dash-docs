903a3baebc84f26243a28d5e59c934512cfccc20
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-17 19:06:21 -0400

    [WIP] header v1

--- app.R
+++ app.R
@@ -8,7 +8,6 @@ css_list <- list("https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/
                  "https://dash.plot.ly/assets/open-sans.css?m=1552356970.0",
                  "https://dash.plot.ly/assets/override.css?m=1552356970.0",
                  "https://dash.plot.ly/assets/pdf-docs.css?m=1552356970.0",
-                 "https://dash.plot.ly/assets/tabs-styled-with-classes.css?m=1552356970.0",
-                 "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css")
+                 "https://dash.plot.ly/assets/tabs-styled-with-classes.css?m=1552356970.0")
 
 app <- Dash$new(external_stylesheets=css_list)
