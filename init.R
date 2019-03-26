969ab6a09b32ecd6976f9d7f7bd65ee258d20a20
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-26 14:57:10 -0400

    [WIP] update pkgs dashR

--- init.R
+++ init.R
@@ -2,10 +2,10 @@
 #
 
 
-install.packages(c("devtools", "fiery", "routr", "base64enc"))
+# install.packages(c("devtools", "fiery", "routr", "base64enc"))
 # #install.packages("/app/localpkgs/dashHtmlComponents_0.13.5.tar.gz", repos=NULL, type="source")
 # #install.packages("/app/localpkgs/dashCoreComponents_0.43.0.tar.gz", repos=NULL, type="source")
 # #install.packages("/app/localpkgs/dashR_0.0.2.tar.gz", repos=NULL, type="source")
-remotes::install_github("plotly/dashR", dependencies = FALSE)
-remotes::install_github("plotly/dash-html-components", dependencies = FALSE)
-remotes::install_github("plotly/dash-core-components", dependencies = FALSE)
+# remotes::install_github("plotly/dashR", dependencies = FALSE)
+# remotes::install_github("plotly/dash-html-components", dependencies = FALSE)
+# remotes::install_github("plotly/dash-core-components", dependencies = FALSE)
