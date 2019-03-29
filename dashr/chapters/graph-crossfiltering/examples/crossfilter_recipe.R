b59d07db9b4077c887039fa81d4a3c012132ebdd
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-03-29 18:58:20 -0400

    :construction_worker: example 3 chapt.5

--- dashr/chapters/graph-crossfiltering/examples/crossfilter_recipe.R
+++ dashr/chapters/graph-crossfiltering/examples/crossfilter_recipe.R
@@ -1,44 +1,44 @@
-library(dashR)
-library(dashCoreComponents)
-library(dashHtmlComponents)
-
-app <- Dash$new()
-
-set.seed(0)
-df <- data.frame(matrix(ncol = 6, nrow = 30))
-x <- c(1:6)
-x <- paste("Column ", x)
-colnames(df) <- x
-
-for (i in 1:6){
-  val <- (i-1)*10
-  nameCol <- paste("Column ", i)
-  for (j in 1:30){
-    df[[i]] <- rnorm(1,val,0.1)
-  }
-}
-
-
-app$layout_set(htmlDiv(list(
-  htmlDiv(
-    dccGraph(
-      id='g1',
-      config=list(displayModeBar = FALSE)
-    ), className='four columns'
-  ),
-  htmlDiv(
-    dccGraph(
-      id='g2',
-      config=list(displayModeBar = FALSE)
-    ), className='four columns'),
-  htmlDiv(
-    dccGraph(
-      id='g3',
-      config=list(displayModeBar = FALSE)
-    ), className='four columns')
-  ), className='row')
-)
-
+# library(dashR)
+# library(dashCoreComponents)
+# library(dashHtmlComponents)
+#
+# app <- Dash$new()
+#
+# set.seed(0)
+# df <- data.frame(matrix(ncol = 6, nrow = 30))
+# x <- c(1:6)
+# x <- paste("Column ", x)
+# colnames(df) <- x
+#
+# for (i in 1:6){
+#   val <- (i-1)*10
+#   nameCol <- paste("Column ", i)
+#   for (j in 1:30){
+#     df[[i]] <- rnorm(1,val,0.1)
+#   }
+# }
+#
+#
+# app$layout_set(htmlDiv(list(
+#   htmlDiv(
+#     dccGraph(
+#       id='g1',
+#       config=list(displayModeBar = FALSE)
+#     ), className='four columns'
+#   ),
+#   htmlDiv(
+#     dccGraph(
+#       id='g2',
+#       config=list(displayModeBar = FALSE)
+#     ), className='four columns'),
+#   htmlDiv(
+#     dccGraph(
+#       id='g3',
+#       config=list(displayModeBar = FALSE)
+#     ), className='four columns')
+#   ), className='row')
+# )
+#
 # highlight <- function(x, y){
 #  callback <- function(selectedDatas){
 #     selectedpoints = colnames(df)
@@ -113,51 +113,51 @@ app$layout_set(htmlDiv(list(
 #     )
 #
 #
-#     # if (selectedDatas[[0]] && selectedDatas[[0]]$range){
-#     #   figure$layout$shapes = list(list(list(
-#     #     'x0' = selectedDatas[[0]]$range$x[[0]],
-#     #     'x1' = selectedDatas[[0]]$range$x[[1]],
-#     #     'y0' = selectedDatas[[0]]$range$y[[0]],
-#     #     'y1' = selectedDatas[[0]]$range$y[[1]]
-#     #   ), shape))
-#     # }else{
-#     #   figure$layout$shapes = list(list(list(
-#     #     'type' = 'rect',
-#     #     'x0' = min(df$x),
-#     #     'x1' = max(df$x),
-#     #     'y0' = min(df$y),
-#     #     'y1' = max(df$y)
-#     #   ), shape))
-#     # }
+#     if (selectedDatas[[0]] && selectedDatas[[0]]$range){
+#       figure$layout$shapes = list(list(list(
+#         'x0' = selectedDatas[[0]]$range$x[[0]],
+#         'x1' = selectedDatas[[0]]$range$x[[1]],
+#         'y0' = selectedDatas[[0]]$range$y[[0]],
+#         'y1' = selectedDatas[[0]]$range$y[[1]]
+#       ), shape))
+#     }else{
+#       figure$layout$shapes = list(list(list(
+#         'type' = 'rect',
+#         'x0' = min(df$x),
+#         'x1' = max(df$x),
+#         'y0' = min(df$y),
+#         'y1' = max(df$y)
+#       ), shape))
+#     }
 #     return(figure)
 #   }
 #   return(callback)
 # }
-
-
-# app.callback is a decorator which means that it takes a function
-# as its argument.
-# highlight is a function "generator": it's a function that returns function
-app$callback(
-  output = list(id='g1', property='figure'),
-  params = list(input(id='g1', property='selectedData'),
-                input(id='g2', property='selectedData'),
-                input(id='g3', property='selectedData'))
-)(highlight('Column 0', 'Column 1'))
-
-app$callback(
-  output = list(id='g2', property='figure'),
-  params = list(input(id='g2', property='selectedData'),
-                input(id='g1', property='selectedData'),
-                input(id='g3', property='selectedData'))
-)(highlight('Column 2', 'Column 3'))
-
-app$callback(
-  output = list(id='g3', property='figure'),
-  params = list(input(id='g3', property='selectedData'),
-                input(id='g1', property='selectedData'),
-                input(id='g2', property='selectedData'))
-)(highlight('Column 4', 'Column 5'))
-
-
-# app$run_heroku()
+#
+#
+# # app.callback is a decorator which means that it takes a function
+# # as its argument.
+# # highlight is a function "generator": it's a function that returns function
+# app$callback(
+#   output = list(id='g1', property='figure'),
+#   params = list(input(id='g1', property='selectedData'),
+#                 input(id='g2', property='selectedData'),
+#                 input(id='g3', property='selectedData'))
+# )(highlight('Column 0', 'Column 1'))
+#
+# app$callback(
+#   output = list(id='g2', property='figure'),
+#   params = list(input(id='g2', property='selectedData'),
+#                 input(id='g1', property='selectedData'),
+#                 input(id='g3', property='selectedData'))
+# )(highlight('Column 2', 'Column 3'))
+#
+# app$callback(
+#   output = list(id='g3', property='figure'),
+#   params = list(input(id='g3', property='selectedData'),
+#                 input(id='g1', property='selectedData'),
+#                 input(id='g2', property='selectedData'))
+# )(highlight('Column 4', 'Column 5'))
+#
+#
+# # app$run_heroku()
