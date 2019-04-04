ba2047820a887cefecb68fa5ed5e9da93f175c72
Nadia Tahiri <nadia.tahiri@gmail.com>
2019-04-04 15:56:29 -0400

    Update init.R

--- init.R
+++ init.R
@@ -1,7 +1,6 @@
 # init.R
 #
 
-<<<<<<< HEAD
 install.packages(c("devtools", "fiery", "routr", "base64enc"))
 #install.packages("/app/localpkgs/dashHtmlComponents_0.13.5.tar.gz", repos=NULL, type="source")
 #install.packages("/app/localpkgs/dashCoreComponents_0.43.0.tar.gz", repos=NULL, type="source")
@@ -9,13 +8,3 @@ install.packages(c("devtools", "fiery", "routr", "base64enc"))
 remotes::install_github("plotly/dashR")
 remotes::install_github("plotly/dash-html-components")
 remotes::install_github("plotly/dash-core-components")
-=======
-# install.packages(c("devtools", "fiery", "routr", "base64enc"))
-
-#install.packages("/app/localpkgs/dashHtmlComponents_0.13.5.tar.gz", repos=NULL, type="source")
-#install.packages("/app/localpkgs/dashCoreComponents_0.43.0.tar.gz", repos=NULL, type="source")
-# install.packages("/app/localpkgs/dashR.tar.gz", repos=NULL, type="source")
-# remotes::install_github("plotly/dashR")
-# remotes::install_github("plotly/dash-html-components")
-# remotes::install_github("plotly/dash-core-components")
->>>>>>> bb1ed4530628c1ee680c68fedb87cdbe1acdb38b
