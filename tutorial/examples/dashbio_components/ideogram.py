d2ca9512e75fc1099ace63137d00c868f9295a6d
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-04-08 16:27:00 -0400

    Make default ideogram example simpler.

--- tutorial/examples/dashbio_components/ideogram.py
+++ tutorial/examples/dashbio_components/ideogram.py
@@ -1,51 +1,42 @@
 import dash
 import dash_bio as dashbio
 import dash_html_components as html
-
+import dash_core_components as dcc
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dashbio.Ideogram(
-        id="my-dashbio-ideogram",
-        dataDir="https://unpkg.com/ideogram@1.3.0/"
-        "dist/data/bands/native/",
-        organism="human",
-        assembly="GRCh37",
-        chromosomes=['X', 'Y'],
-        orientation="horizontal",
-        showBandLabels=True,
-        chrHeight=400,
-        rotatable=False,
-        annotationsPath='https://eweitz.github.io/ideogram/data/annotations/SRR562646.json'
+    'Select which chromosomes to display on the ideogram below:',
+    dcc.Dropdown(
+        id='displayed-chromosomes',
+        options=[{'label': str(i), 'value': str(i)} for i in range(1, 23)],
+        multi=True,
+        value=[str(i) for i in range(1, 23)]
     ),
-    html.Div(id='ideogram-output')
+    dashbio.Ideogram(
+        id='my-dashbio-ideogram'
+    ),
+    html.Div(id='ideogram-rotated')
 ])
 
 
 @app.callback(
-    dash.dependencies.Output('ideogram-output', 'children'),
-    [dash.dependencies.Input('my-dashbio-ideogram', 'annotationsData')]
+    dash.dependencies.Output('my-dashbio-ideogram', 'chromosomes'),
+    [dash.dependencies.Input('displayed-chromosomes', 'value')]
 )
-def show_annotations(annot):
-    if annot is None or len(annot) == 0:
-        return ''
-    annot_info = annot.split('<br>')
-    gene_name = annot_info[0]
-    gene_location_info = annot_info[1].split(':')
-    chromosome_name = gene_location_info[0].replace('chr', '')
-    basepair_location = gene_location_info[1]
+def update_ideogram(value):
+    return value
 
-    return [
-        "Hover information:",
-        html.Div('Gene name: {}'.format(gene_name)),
-        html.Div('Location: chromosome {}, base pairs {}'.format(
-            chromosome_name,
-            basepair_location
-        ))
-    ]
+
+@app.callback(
+    dash.dependencies.Output('ideogram-rotated', 'children'),
+    [dash.dependencies.Input('my-dashbio-ideogram', 'rotated')]
+)
+def update_ideogram_rotated(rot):
+    return 'You have {} selected a chromosome.'.format(
+        '' if rot else 'not')
 
 
 if __name__ == '__main__':
