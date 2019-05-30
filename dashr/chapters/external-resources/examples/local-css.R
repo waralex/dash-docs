ad09768037691f266a35f8609e56c164db5cc9f0
z267xu <40337165+z267xu@users.noreply.github.com>
2019-05-30 10:54:54 -0700

    Nothing changed yet

---
+++ dashr/chapters/external-resources/examples/local-css.R
@@ -0,0 +1,25 @@
+library(dashR)
+library(dashCoreComponents)
+library(dashHtmlComponents)
+
+app <- Dash$new()
+
+app$layout(htmlDiv(list(
+    htmlDiv(
+        className="app-header",
+        children=list(
+            htmlDiv('Plotly Dash', className="app-header--title")
+        )
+    ),
+    htmlDiv(
+        children=htmlDiv(list(
+            htmlH5('Overview'),
+            htmlDiv("
+                This is an example of a simple Dash app with
+                local, customized CSS.
+            ")
+        ))
+    )
+)))
+
+# app$run_heroku()
