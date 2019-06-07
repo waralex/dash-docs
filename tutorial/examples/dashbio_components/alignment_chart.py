19a8faf87cbaa756c890b6646913e6b76c5c63f4
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-07 16:32:22 -0400

    Fix URL for alignment chart data.

--- tutorial/examples/dashbio_components/alignment_chart.py
+++ tutorial/examples/dashbio_components/alignment_chart.py
@@ -11,7 +11,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 data = urlreq.urlopen(
-    'https://raw.githubusercontent.com/plotly/dash-bio/master/' +
+    'https://raw.githubusercontent.com/plotly/dash-bio-docs-files/master/' +
     'alignment_viewer_p53.fasta'
 ).read()
 
