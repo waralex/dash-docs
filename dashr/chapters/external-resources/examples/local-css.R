b7df1b487133cc540f72a6f091a752d36ef1e485
z267xu <40337165+z267xu@users.noreply.github.com>
2019-06-10 16:28:00 -0400

    Chapter external-resources has been partially edited (From beginning to `Adding external CSS/Javascript`).
    TODO:
    The rest part will be edited when corresponding `dashR` is completed (like `meta_tags`, `index_string`, `render` and etc).

--- dashr/chapters/external-resources/examples/local-css.R
+++ dashr/chapters/external-resources/examples/local-css.R
@@ -1,25 +1,28 @@
 library(dashR)
 library(dashCoreComponents)
 library(dashHtmlComponents)
-
 app <- Dash$new()
 
-app$layout(htmlDiv(list(
-    htmlDiv(
-        className="app-header",
-        children=list(
-            htmlDiv('Plotly Dash', className="app-header--title")
+app$layout(
+  htmlDiv(
+    list(
+      htmlDiv(
+        className = "app-header",
+        children = list(
+          htmlDiv('Plotly Dash', className = "app-header--title")
         )
-    ),
-    htmlDiv(
-        children=htmlDiv(list(
-            htmlH5('Overview'),
-            htmlDiv("
+      ),
+      htmlDiv(
+        list(
+          htmlH5('Overview'),
+          htmlDiv(" 
                 This is an example of a simple Dash app with
                 local, customized CSS.
             ")
-        ))
+        )
+      )
     )
-)))
+  )
+)
 
-# app$run_heroku()
+app$run_server()
