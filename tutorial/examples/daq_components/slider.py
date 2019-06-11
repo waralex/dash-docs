cf1c44d8322abc3024747feb50638c0e91a90e97
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-11 15:18:14 -0400

    Reformat all daq files with black.

--- tutorial/examples/daq_components/slider.py
+++ tutorial/examples/daq_components/slider.py
@@ -2,25 +2,22 @@ import dash
 import dash_daq as daq
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    daq.Slider(
-        id='my-daq-slider',
-        value=17
-    ),
-    html.Div(id='slider-output')
-])
+app.layout = html.Div(
+    [daq.Slider(id="my-daq-slider", value=17), html.Div(id="slider-output")]
+)
 
 
 @app.callback(
-    dash.dependencies.Output('slider-output', 'children'),
-    [dash.dependencies.Input('my-daq-slider', 'value')])
+    dash.dependencies.Output("slider-output", "children"),
+    [dash.dependencies.Input("my-daq-slider", "value")],
+)
 def update_output(value):
-    return 'The slider is currently at {}.'.format(value)
+    return "The slider is currently at {}.".format(value)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
--- tutorial/examples/daq_components/toggle_switch.py
+++ tutorial/examples/daq_components/toggle_switch.py
@@ -2,24 +2,25 @@ import dash
 import dash_daq as daq
 import dash_html_components as html
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([
-    daq.ToggleSwitch(
-        id='my-toggle-switch',
-        value=False
-    ),
-    html.Div(id='toggle-switch-output')
-])
+app.layout = html.Div(
+    [
+        daq.ToggleSwitch(id="my-toggle-switch", value=False),
+        html.Div(id="toggle-switch-output"),
+    ]
+)
+
 
 @app.callback(
-    dash.dependencies.Output('toggle-switch-output', 'children'),
-    [dash.dependencies.Input('my-toggle-switch', 'value')])
+    dash.dependencies.Output("toggle-switch-output", "children"),
+    [dash.dependencies.Input("my-toggle-switch", "value")],
+)
 def update_output(value):
-    return 'The switch is {}.'.format(value)
+    return "The switch is {}.".format(value)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
