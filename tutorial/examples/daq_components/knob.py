5395fe93f1e4cc93da7bd82ef5845fd6b9e1616d
Shammamah Hossain <shammamah.hossain@gmail.com>
2019-06-11 15:20:19 -0400

    Revert "Reformat all daq files with black."
    This reverts commit cf1c44d8322abc3024747feb50638c0e91a90e97.

--- tutorial/examples/daq_components/knob.py
+++ tutorial/examples/daq_components/knob.py
@@ -2,20 +2,24 @@ import dash
 import dash_daq as daq
 import dash_html_components as html
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-app.layout = html.Div([daq.Knob(id="my-knob"), html.Div(id="knob-output")])
+app.layout = html.Div([
+    daq.Knob(
+        id='my-knob',
+    ),
+    html.Div(id='knob-output')
+])
 
 
 @app.callback(
-    dash.dependencies.Output("knob-output", "children"),
-    [dash.dependencies.Input("my-knob", "value")],
-)
+    dash.dependencies.Output('knob-output', 'children'),
+    [dash.dependencies.Input('my-knob', 'value')])
 def update_output(value):
-    return "The knob value is {}.".format(value)
+    return 'The knob value is {}.'.format(value)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run_server(debug=True)
