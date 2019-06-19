352efd3d0689bfcff0db90e0dbc4cda30df504be
alexcjohnson <johnson.alex.c@gmail.com>
2019-06-19 11:49:36 -0400

    remove `column.hidden`
    https://github.com/plotly/dash-table/pull/473

--- tutorial/table/styling_chapter.py
+++ tutorial/table/styling_chapter.py
@@ -243,7 +243,7 @@ layout = html.Div(
                 {"name": ["", "Year"], "id": "year"},
                 {"name": ["City", "Montreal"], "id": "montreal"},
                 {"name": ["City", "Toronto"], "id": "toronto"},
-                {"name": ["City", "Ottawa"], "id": "ottawa", "hidden": True},
+                {"name": ["City", "Ottawa"], "id": "ottawa"},
                 {"name": ["City", "Vancouver"], "id": "vancouver"},
                 {"name": ["Climate", "Temperature"], "id": "temp"},
                 {"name": ["Climate", "Humidity"], "id": "humidity"},
