af0ce145b54da92f0f1b3462b63a9737c5ce3368
byron <byron@plot.ly>
2019-09-11 09:05:20 -0400

    add cytoscape

--- tests/snapshots/test_dcc_pages.py
+++ tests/snapshots/test_dcc_pages.py
@@ -18,11 +18,21 @@ def test_snap300_dds_resources(chapter_map, dash_doc):
         )
 
 
-def test_snap400_dash_table_resources(chapter_map, dash_doc):
+def test_snap400_dashtable_resources(chapter_map, dash_doc):
     table_resources = [
         _ for _ in chapter_map if _.startswith("/datatable/")
     ]
     for resource in table_resources:
+        dash_doc.visit_and_snapshot(
+            resource, hook_id="wait-for-page-{}".format(resource)
+        )
+
+
+def test_snap500_cytoscape_resources(chapter_map, dash_doc):
+    cytoscapes = [
+        _ for _ in chapter_map if _.startswith("/cytoscape/")
+    ]
+    for resource in cytoscapes:
         dash_doc.visit_and_snapshot(
             resource, hook_id="wait-for-page-{}".format(resource)
         )
