8473683a8ae577a3a7fb156516aa65bc3c656680
byron <byron@plot.ly>
2019-09-11 11:13:01 -0400

    get rid of extra dcc

--- tests/snapshots/test_dcc_pages.py
+++ tests/snapshots/test_dcc_pages.py
@@ -1,6 +1,6 @@
 def test_snap200_dcc_resources(chapter_map, dash_doc):
     dcc_resources = [
-        _ for _ in chapter_map if _.startswith("/dash-core-components")
+        _ for _ in chapter_map if _.startswith("/dash-core-components/")
     ]
     for resource in dcc_resources:
         dash_doc.visit_and_snapshot(
@@ -10,7 +10,7 @@ def test_snap200_dcc_resources(chapter_map, dash_doc):
 
 def test_snap300_dds_resources(chapter_map, dash_doc):
     dds_resources = [
-        _ for _ in chapter_map if _.startswith("/dash-deployment-server")
+        _ for _ in chapter_map if _.startswith("/dash-deployment-server/")
     ]
     for resource in dds_resources:
         dash_doc.visit_and_snapshot(
@@ -19,9 +19,7 @@ def test_snap300_dds_resources(chapter_map, dash_doc):
 
 
 def test_snap400_dashtable_resources(chapter_map, dash_doc):
-    table_resources = [
-        _ for _ in chapter_map if _.startswith("/datatable/")
-    ]
+    table_resources = [_ for _ in chapter_map if _.startswith("/datatable/")]
     for resource in table_resources:
         dash_doc.visit_and_snapshot(
             resource, hook_id="wait-for-page-{}".format(resource)
@@ -29,9 +27,7 @@ def test_snap400_dashtable_resources(chapter_map, dash_doc):
 
 
 def test_snap500_cytoscape_resources(chapter_map, dash_doc):
-    cytoscapes = [
-        _ for _ in chapter_map if _.startswith("/cytoscape/")
-    ]
+    cytoscapes = [_ for _ in chapter_map if _.startswith("/cytoscape/")]
     for resource in cytoscapes:
         dash_doc.visit_and_snapshot(
             resource, hook_id="wait-for-page-{}".format(resource)
--- tests/snapshots/test_main_page.py
+++ tests/snapshots/test_main_page.py
@@ -5,11 +5,11 @@ def test_snap001_index_page_links(dash_doc):
         elem.get_property("id")
         for elem in dash_doc.driver.find_elements_by_tag_name("a")
     )
-    resources = [
+    resources = {
         id_
         for id_ in ids
         if id_.startswith("/") and id_ != "/external-resources"
-    ]
+    }
     for resource in resources:
         dash_doc.visit_and_snapshot(
             resource, hook_id="wait-for-page-{}".format(resource)
