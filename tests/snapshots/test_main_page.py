29fb5b3069dbc4add48e0963a023d8625fecc104
byron <byron@plot.ly>
2019-09-11 10:42:17 -0400

    :bug: duplicate snapshot key for external

--- tests/snapshots/test_main_page.py
+++ tests/snapshots/test_main_page.py
@@ -8,7 +8,7 @@ def test_snap001_index_page_links(dash_doc):
     resources = [
         id_
         for id_ in ids
-        if id_.startswith("/") or id_ == "/external-resources"
+        if id_.startswith("/") and id_ != "/external-resources"
     ]
     for resource in resources:
         dash_doc.visit_and_snapshot(
