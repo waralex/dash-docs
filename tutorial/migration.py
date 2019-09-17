2fb9d177bc97438b02e7f9c16cf9fb10c16d30e3
michaelbabyn <michael@plot.ly>
2019-09-17 11:56:35 -0400

    add note re: append_script wrt serve_locally=True to 1.0 migration guide

--- tutorial/migration.py
+++ tutorial/migration.py
@@ -50,6 +50,10 @@ layout = html.Div([
     ```
     app = dash.Dash(serve_locally=False)
     ```
+    
+    > **Note that using `app.scripts.append_script` to load external js scripts
+    > won't work while `serve_locally=True`. Please use the `external_scripts` argument
+    > in your call to `dash.Dash` instead, as outlined in [the external resources section](/external-resources).**
 
     ### Removed undo/redo buttons
     The undo & redo buttons in the corner of every app are removed by default,
