df84dc346ff3a4c6f4f1cd7aca9cd7cdc94fb4ed
CanerIrfanoglu <canerirfanoglu@gmail.com>
2019-10-16 09:03:20 -0400

    :art: 4/6 examples & wip 2/6

--- dashr/chapters/dash-canvas/examples/canvas_annotations.R
+++ dashr/chapters/dash-canvas/examples/canvas_annotations.R
@@ -6,41 +6,48 @@ library(jsonlite)
 library(magrittr)
 library(compiler)
 
-
 filename <- 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Mitochondria%2C_mammalian_lung_-_TEM_%282%29.jpg'
-canvas_width <- 300 
+canvas_width <- 500
 columns <- c("type", "width", "height", "scaleX", "strokeWidth", "path")
 
 app <- Dash$new()
 
 app$layout(htmlDiv(list(
-
-  htmlH6('Draw on image and press Save to show annotations geometry')
-  ,
+  htmlH6('Draw on image and press Save to show annotations geometry'),
   htmlDiv(list(
-
-    dashCanvas(id='annot-canvas', lineWidth=5, filename=filename, width=canvas_width)
-
+    dashCanvas(
+      id = 'annot-canvas',
+      lineWidth = 5,
+      filename = filename,
+      width = canvas_width
+    )
   )),
-  dashDataTable(id='annot-canvas-table',
-                style_cell = list(textAlign='left'),
-                columns = lapply(columns, function(col){return(list(name=col, id=col))})
-                )
- 
+  htmlDiv(
+    dashDataTable(
+      id = 'annot-canvas-table',
+      style_table = list('overflowX' = 'scroll'),
+      style_cell = list(textAlign = 'left'),
+      columns = lapply(columns, function(col) {
+        return(list(name = col, id = col))
+      })
+    )
+  )
 )))
 
 app$callback(
   output = list(id='annot-canvas-table', property='data'),
   params = list(input(id='annot-canvas',property='json_data')),
-  function(json)
-  {
-    if(is.null(json)||is.na(json)){return(list(0,0,0,0,0,0))} else{
-    data = fromJSON(json)
-    return(data[['objects']][-1])
+
+  function(json) {
+    if (is.null(json[[1]]) || is.na(json[[1]])) {
+      return(list(0, 0, 0, 0, 0, 0))
+    } else {
+      data <- fromJSON(json)
+      df <- data[['objects']]
+      df <- df[2:nrow(df), ]
+      return(df)
     }
   }
 )
 
-#app$run_server()
-  
-
+app$run_server()
--- dashr/chapters/dash-canvas/examples/canvas_color.R
+++ dashr/chapters/dash-canvas/examples/canvas_color.R
@@ -11,27 +11,32 @@ canvas_width <- 300
 app <- Dash$new()
 
 app$layout(htmlDiv(list(
-  
-  htmlDiv(dashCanvas(id='canvas-color', width=canvas_width, filename=filename, hide_buttons=list('line', 'zoom', 'pan')), className='six columns')
-  ,
-  htmlDiv(list(
-    
-    htmlH6(children='Brush width')
-    ,
-    dccSlider(id='bg-width-slider', min=2, max=40, step=1, value=5)
-    ,
-    daqColorPicker(id='color-picker', label='Brush color', value='#119DFF')
-    
-  ), className='three columns')
-  
-)))
+  htmlDiv(
+    dashCanvas(
+      id = 'canvas-color',
+      width = canvas_width,
+      filename = filename,
+      hide_buttons = list('line', 'zoom', 'pan')
+    ),
+    className = 'six columns'
+  ),
+  htmlDiv(
+    list(
+      htmlH6(children = 'Brush width'),
+      dccSlider(
+        id = 'bg-width-slider',
+        min = 2,
+        max = 40,
+        step = 1,
+        value = 5
+      ),
+      daqColorPicker(id = 'color-picker', label = 'Brush color', value = '#119DFF')
+    ), className = 'three columns')
+)), className = "row")
 
 app$callback(
-  
-  output=list(id='canvas-color', property='lineColor')
-  ,
-  params=list(input(id='color-picker', property='value'))
-  ,
+  output=list(id='canvas-color', property='lineColor'),
+  params=list(input(id='color-picker', property='value')),
   function(value){
     if(class(value)=='list'){
       return(value[['hex']])
@@ -39,19 +44,14 @@ app$callback(
       return(value)
     }
   }
-  
 )
 
 app$callback(
-  
-  output=list(id='canvas-color', property='lineWidth')
-  ,
-  params=list(input(id='bg-width-slider', property='value'))
-  ,
+  output=list(id='canvas-color', property='lineWidth'),
+  params=list(input(id='bg-width-slider', property='value')),
   function(value){
     return(value)
   }
-  
 )
 
-#app$run_server()
+app$run_server()
