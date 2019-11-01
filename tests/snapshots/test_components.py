9124b3db8b2d5fb29533815fd49082dec4db178a
byron <byron@plot.ly>
2019-11-01 14:53:08 -0400

    udpate

--- tests/snapshots/test_components.py
+++ tests/snapshots/test_components.py
@@ -1,8 +1,7 @@
 def test_snap100_component_sub_pages(component_map, dash_doc):
     for _, grouper in component_map:
         for resources in grouper:
-            print(resources)
             href = "/".join(resources)
             dash_doc.visit_and_snapshot(
-                href, hook_id="wait-for-page-/{}".format(href)
+                href, hook_id="wait-for-page-/{}".format(href), assert_check=False
             )
