3950ff55bd39ee2436fe1584575b58d573680e86
byron <byron@plot.ly>
2019-11-04 14:05:24 -0500

    skip wrong percy ones

--- tests/snapshots/test_components.py
+++ tests/snapshots/test_components.py
@@ -3,6 +3,13 @@ def test_snap100_component_sub_pages(component_map, dash_doc):
         for resources in grouper:
             href = "/".join(resources)
             dash_doc.visit_and_snapshot(
-                href, hook_id="wait-for-page-/{}".format(href),
-                assert_check=True
+                href,
+                hook_id="wait-for-page-/{}".format(href),
+                wait_for_callbacks=(
+                    False
+                    if href
+                    in {"dash-core-components/store", "datatable/callbacks"}
+                    else True
+                ),
+                assert_check=True,
             )
