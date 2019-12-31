a27b0d9bc14088fadde36f8bf7e48150cbbe2307
Ryan Patrick Kyle <ryan@plot.ly>
2019-12-31 02:27:42 -0500

    :label: add meta tag

--- app.R
+++ app.R
@@ -1,3 +1,9 @@
 library(dash)
 
-app <- Dash$new(name = "Dash User Guide and Documentation - Dash by Plotly")
+metas <- list(
+  list(name = 'description', 
+       content = 'Dash User Guide and Documentation. Dash is a framework for building analytical web apps in Python and R.')
+)
+
+app <- Dash$new(name = "Dash User Guide and Documentation - Dash by Plotly",
+                meta_tags = metas)
