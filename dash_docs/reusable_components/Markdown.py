86e5e8f9421bd62e4914b32a038c9bdd733815c2
Chris P <chris@plot.ly>
2020-01-27 12:51:42 -0500

    :wrench: include relative path & tests

--- dash_docs/reusable_components/Markdown.py
+++ dash_docs/reusable_components/Markdown.py
@@ -2,14 +2,23 @@ import dash_core_components as dcc
 from dash_docs import tools
 import re
 
-# Use our own Markdown function so that we can
-# use `relpath` for the embedded URLs
-def Markdown(children='', **kwargs):
+def replace_relative_links(children):
     children = re.sub(
         ']\((\/\S*)\)',
         lambda match: ']({})'.format(tools.relpath(match.groups()[0])),
         children
     )
+    children = re.sub(
+        'href="(/\S*)"',
+        lambda match: 'href="{}"'.format(tools.relpath(match.groups()[0])),
+        children
+    )
+    return children
+
+# Use our own Markdown function so that we can
+# use `relpath` for the embedded URLs
+def Markdown(children='', **kwargs):
+    children = replace_relative_links(children)
     if kwargs.pop('escape_tags', False) and 'dccLink' in children:
         # escape the HTML tags presented in the html component docstrings
         # note that if these tags are within `backticks`, then we don't need
