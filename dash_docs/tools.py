1f583fca01c45ecca8a39e788ba62771261967ab
Marc-André Rivet <Marc-Andre-Rivet@users.noreply.github.com>
2020-02-19 11:06:31 -0500

    prevent double relative paths (#786)

--- dash_docs/tools.py
+++ dash_docs/tools.py
@@ -8,7 +8,7 @@ else:
 
 
 def relpath(path):
-    if path.startswith('/') and 'DASH_DOCS_URL_PREFIX' in os.environ:
+    if path.startswith('/') and 'DASH_DOCS_URL_PREFIX' in os.environ and not path.startswith('/{}/'.format(os.environ['DASH_DOCS_URL_PREFIX'].rstrip('/'))):
         # In enterprise docs, all assets are under `/Docs`
         return '{}{}'.format(
             os.environ['DASH_DOCS_URL_PREFIX'].rstrip('/'),
