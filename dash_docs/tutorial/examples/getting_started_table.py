463821f33ebc111a19f6cac718dbbae831e5bd95
alexcjohnson <johnson.alex.c@gmail.com>
2020-03-07 08:48:47 -0500

    put back max_rows and remove comments from getting_started_table

--- dash_docs/tutorial/examples/getting_started_table.py
+++ dash_docs/tutorial/examples/getting_started_table.py
@@ -8,19 +8,13 @@ df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
-
-        # Header
         html.Thead(
-            html.Tr([
-                html.Th(col) for col in dataframe.columns
-            ])
+            html.Tr([html.Th(col) for col in dataframe.columns])
         ),
-
-        # Body
         html.Tbody([
             html.Tr([
                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(len(dataframe))
+            ]) for i in range(min(len(dataframe), max_rows))
         ])
     ])
 
