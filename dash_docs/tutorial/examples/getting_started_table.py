f35cd910ace968de0aa4828fd25d2539f547f16e
YaCpotato <yasshisshy@gmail.com>
2020-03-07 12:05:25 +0900

    apply comments

--- dash_docs/tutorial/examples/getting_started_table.py
+++ dash_docs/tutorial/examples/getting_started_table.py
@@ -8,11 +8,15 @@ df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/c78bf172206ce24f77
 
 def generate_table(dataframe, max_rows=10):
     return html.Table([
+
+        # Header
         html.Thead(
             html.Tr([
                 html.Th(col) for col in dataframe.columns
             ])
         ),
+
+        # Body
         html.Tbody([
             html.Tr([
                 html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
