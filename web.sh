132cf17f15520bfe74a7796726ec844628dd6925
Ryan Patrick Kyle <ryan@plot.ly>
2020-03-09 16:08:54 -0400

    use Python by default/if no env var

--- web.sh
+++ web.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-if [ "$LANGUAGE" = "PYTHON" ]
-then gunicorn app:runserver
-else fakechroot fakeroot chroot /app/.root /bin/sh -c '/usr/bin/R -f /app/run.R --gui-none --no-save'
+if [ "$LANGUAGE" = "R" ]
+then fakechroot fakeroot chroot /app/.root /bin/sh -c '/usr/bin/R -f /app/run.R --gui-none --no-save'
+else gunicorn app:runserver
 fi
