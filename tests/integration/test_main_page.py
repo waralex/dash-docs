fb531638fb23980c943c9a313bdede5c56abd964
Chris P <chris@plot.ly>
2020-03-17 16:21:24 -0700

    relax the test

--- tests/integration/test_main_page.py
+++ tests/integration/test_main_page.py
@@ -41,7 +41,7 @@ def test_snap001_index_page_links(dash_doc, index_pages):
                 '.map(a=>a.attributes.href.value)'
             )
             for link in linked_paths:
-                if link not in URL_TO_CONTENT_MAP and link not in good_links:
+                if link.rstrip('/') not in URL_TO_CONTENT_MAP and link not in good_links:
                     msg = '{} --- on page {}'.format(link, resource)
                     logger.info(msg)
                     bad_links.append(msg)
