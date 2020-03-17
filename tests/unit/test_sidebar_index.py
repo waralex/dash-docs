6cc22a20e12a74dd0fe01365ee9a19d99f5186c2
Chris P <chris@plot.ly>
2020-03-17 14:32:09 -0700

    skip python3 unit test

--- tests/unit/test_sidebar_index.py
+++ tests/unit/test_sidebar_index.py
@@ -1,5 +1,7 @@
+import pytest
 from generate_sidebar_index import generate_sidebar_index
 
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or higher")
 def test_sidebar_index_is_updated():
     with open('SIDEBAR-INDEX.json', 'r') as f:
         saved_sidebar_index = f.read()
