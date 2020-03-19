8f01cb73dff69faf3b27e1bc3033e53c783bbab7
Ryan Patrick Kyle <rpkyle@users.noreply.github.com>
2020-03-18 22:12:43 -0400

    Update app.R
    Co-Authored-By: Chris Parmer <chris@plot.ly>

--- app.R
+++ app.R
@@ -2,7 +2,7 @@ library(dash)
 
 metas <- list(
   list(name = 'description', 
-       content = 'Dash User Guide and Documentation. Dash is a framework for building analytical web apps in Python and R.')
+       content = 'Dash for R User Guide and Documentation. Dash is a framework for building analytical web apps in R and Python.')
 )
 
 app <- Dash$new(assets_folder="dash_docs/assets", meta_tags = metas)
