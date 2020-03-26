89d08d54b9c80d491a5615ae2fb6f2d9cad1a555
wbrgss <williamb@plot.ly>
2020-03-26 13:58:14 -0400

    Move tutorial/components to reusable_components

---
+++ dash_docs/reusable_components/Syntax.py
@@ -0,0 +1,19 @@
+import dash_html_components as html
+
+from dash_docs import styles
+from . import Markdown
+
+from textwrap import dedent
+
+def Syntax(children, style=styles.code_container, summary=''):
+    code = dedent(children).strip()
+    if not code.startswith('```'):
+        code = '```py\n' + code.strip() + '\n```'
+
+    if summary:
+        return html.Details([
+            html.Summary(summary),
+            Markdown(code, style=style)
+        ], open=True)
+    else:
+        return Markdown(code, style=style)
