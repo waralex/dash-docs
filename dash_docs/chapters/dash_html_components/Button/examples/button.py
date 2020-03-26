5815a3cc2fa68520d9a4ab0be850c68e619caa97
Chris P <chris@plot.ly>
2020-03-26 14:54:24 -0700

    another duplicate id fix

--- dash_docs/chapters/dash_html_components/Button/examples/button.py
+++ dash_docs/chapters/dash_html_components/Button/examples/button.py
@@ -7,7 +7,7 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div([
     html.Div(dcc.Input(id='submit-input-box', type='text')),
-    html.Button('Submit', id='submit-button'),
+    html.Button('Submit', id='submit-button-example'),
     html.Div(id='submit-output-button',
              children='Enter a value and press submit')
 ])
@@ -15,7 +15,7 @@ app.layout = html.Div([
 
 @app.callback(
     dash.dependencies.Output('submit-output-button', 'children'),
-    [dash.dependencies.Input('submit-button', 'n_clicks')],
+    [dash.dependencies.Input('submit-button-example', 'n_clicks')],
     [dash.dependencies.State('submit-input-box', 'value')])
 def update_output(n_clicks, value):
     return 'The input value was "{}" and the button has been clicked {} times'.format(
