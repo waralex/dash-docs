befc9b03b7d39236d0d5d2b4355ea105f639caa4
Joseph Damiba <joseph.damiba@plot.ly>
2020-06-04 15:21:28 -0400

    update first callback example to use px

--- dash_docs/chapters/basic_callbacks/examples/getting_started_graph.py
+++ dash_docs/chapters/basic_callbacks/examples/getting_started_graph.py
@@ -2,10 +2,11 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.express as px
 
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+df = px.data.gapminder()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -29,34 +30,13 @@ app.layout = html.Div([
     [Input('year-slider', 'value')])
 def update_figure(selected_year):
     filtered_df = df[df.year == selected_year]
-    traces = []
-    for i in filtered_df.continent.unique():
-        df_by_continent = filtered_df[filtered_df['continent'] == i]
-        traces.append(dict(
-            x=df_by_continent['gdpPercap'],
-            y=df_by_continent['lifeExp'],
-            text=df_by_continent['country'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-                'size': 15,
-                'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
+    
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp", animation_frame="year",
+                     animation_group="country",size="pop", color="continent", hover_name="country", log_x=True, size_max=55, range_x=[100,100000], range_y=[25,90])
+    
+    fig.update_layout(transition_duration=500)
 
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range':[2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition = {'duration': 500},
-        )
-    }
+    return fig
 
 
 if __name__ == '__main__':
