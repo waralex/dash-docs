bc1969014ba688fdf6985cce731199740d4f170f
Joseph Damiba <joseph.damiba@plot.ly>
2020-06-08 12:37:08 -0400

    limit data to single year

--- dash_docs/chapters/getting_started/examples/getting_started_viz.py
+++ dash_docs/chapters/getting_started/examples/getting_started_viz.py
@@ -10,9 +10,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = px.data.gapminder()
 
-fig = px.scatter(df, x="gdpPercap", y="lifeExp", animation_frame="year", animation_group="country",
-           size="pop", color="continent", hover_name="country", facet_col="continent",
-           log_x=True, size_max=45, range_x=[100,100000], range_y=[25,90])
+fig = px.scatter(df.query("year==2007"), x="gdpPercap", y="lifeExp", 
+                 size="pop", color="continent", hover_name="country", log_x=True, size_max=60)
 
 app.layout = html.Div([
     dcc.Graph(
