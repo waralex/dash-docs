63cffa75d91b52a934ed350de49286f3fe286d4b
Chris P <chris@plot.ly>
2020-06-12 11:56:23 -0700

    new build

--- dash-user-guide-components/dash_user_guide_components/Sidebar.py
+++ dash-user-guide-components/dash_user_guide_components/Sidebar.py
@@ -8,16 +8,17 @@ class Sidebar(Component):
 
 
 Keyword arguments:
+- children (a list of or a singular dash component, string or number; optional): Custom content in the "no results found" panel
 - id (string; optional): The ID used to identify this component in Dash callbacks.
 - urls (list; optional): URLs
 - depth (number; default 0): depth"""
     @_explicitize_args
-    def __init__(self, id=Component.UNDEFINED, urls=Component.UNDEFINED, depth=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['id', 'urls', 'depth']
+    def __init__(self, children=None, id=Component.UNDEFINED, urls=Component.UNDEFINED, depth=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['children', 'id', 'urls', 'depth']
         self._type = 'Sidebar'
         self._namespace = 'dash_user_guide_components'
         self._valid_wildcard_attributes =            []
-        self.available_properties = ['id', 'urls', 'depth']
+        self.available_properties = ['children', 'id', 'urls', 'depth']
         self.available_wildcard_properties =            []
 
         _explicit_args = kwargs.pop('_explicit_args')
@@ -29,4 +30,4 @@ Keyword arguments:
             if k not in args:
                 raise TypeError(
                     'Required argument `' + k + '` was not specified.')
-        super(Sidebar, self).__init__(**args)
+        super(Sidebar, self).__init__(children=children, **args)
