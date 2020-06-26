11891250529d0087dbd7e47ad776d9beb6ceb23d
Joseph Damiba <Joseph.damiba@plot.ly>
2020-06-25 20:50:46 -0400

    Update dash_docs/chapters/graph_crossfiltering/examples/getting_started_crossfilter.py
    Co-authored-by: alexcjohnson <johnson.alex.c@gmail.com>

--- dash_docs/chapters/graph_crossfiltering/examples/getting_started_crossfilter.py
+++ dash_docs/chapters/graph_crossfiltering/examples/getting_started_crossfilter.py
@@ -110,8 +110,8 @@ def create_time_series(dff, axis_type, title):
     fig.update_yaxes(type='linear' if axis_type == 'Linear' else 'log')
 
     fig.add_annotation(x=0, y=0.85, xanchor='left', yanchor='bottom',
-                           xref='paper', yref='paper', showarrow=False, align='left',
-                           bgcolor='rgba(255, 255, 255, 0.5)', text=title)
+                       xref='paper', yref='paper', showarrow=False, align='left',
+                       bgcolor='rgba(255, 255, 255, 0.5)', text=title)
 
     fig.update_layout(height=225, margin={'l': 20, 'b': 30, 'r': 10, 't': 10})
 
