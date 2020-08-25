15c992b3aa4605b257d40ba68173a3e44887df48
John Bampton <jbampton@users.noreply.github.com>
2020-08-26 06:52:42 +1000

    Fix spelling

--- dash_docs/chapter_index.py
+++ dash_docs/chapter_index.py
@@ -227,7 +227,7 @@ URLS = [
             {
                 'url': '/callback-gotchas',
                 'name': 'Callback Gotchas',
-                'description': 'Dash callbacks have some idiosyncracies that '
+                'description': 'Dash callbacks have some idiosyncrasies that '
                 'should be taken into consideration when building a Dash app. '
                 'If you\'re running into unexpected callback behavior, '
                 'and the rest of the documentation hasn\'t shed any light on '
@@ -892,7 +892,7 @@ URLS = [
                     {
                         'url': '/dash-enterprise/configure-system-dependencies',
                         'content': chapters.dash_enterprise.dash_enterprise_chapters.ConfigSys,
-                        'name': 'Configuring System Dependencie',
+                        'name': 'Configuring System Dependency',
                         'description': 'Install and configure system dependencies such '
                         'as database drivers or the Java JRE environment.'
                     },
@@ -923,7 +923,7 @@ URLS = [
                     {
                         'url': '/dash-enterprise/environment-variables',
                         'content': chapters.dash_enterprise.dash_enterprise_chapters.EnvVars,
-                        'name': 'Setting Enviornment Variables',
+                        'name': 'Setting Environment Variables',
                         'description': 'Environment variables are commonly used to store '
                         'secret variables like database passwords.'
                     },
@@ -964,7 +964,7 @@ URLS = [
                         'url': '/dash-enterprise/private-packages',
                         'content': chapters.dash_enterprise.dash_enterprise_chapters.PrivatePackages,
                         'name': 'Adding Private Python Packages',
-                        'description': 'Intsall private python packages in your Dash Apps.'
+                        'description': 'Install private python packages in your Dash Apps.'
                     },
                     {
                         'url': '/dash-enterprise/celery-process',
@@ -977,7 +977,7 @@ URLS = [
                         'content': chapters.dash_enterprise.dash_enterprise_chapters.StagingApp,
                         'name': 'Create a Staging Dash App ',
                         'description': 'Use a staged Dash App to test changes before updating '
-                        'your prodcution Dash App.'
+                        'your production Dash App.'
                     },
                     {
                         'url': '/dash-enterprise/pdf-service',
