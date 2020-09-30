78343072b936f716b167856a1772499ef6f515fc
rpkyle <ryan@plotly.com>
2020-09-29 22:40:41 -0400

    update utils.jl

--- dash_docs/utils.jl
+++ dash_docs/utils.jl
@@ -6,7 +6,7 @@ function LoadExampleCode(filename, wd = nothing)
   replacements = [
       r"app.layout =" => "layout ="
       r"app =.*dash\((.*?)\)"m => ""
-      r"run_server(app, \"0.0.0.0\", 8000)" => ""
+      r"run_server\(.*?\)" => ""
   ]
   for replacement in replacements
     example_ready_for_eval = replace(example_ready_for_eval, replacement)
@@ -20,14 +20,15 @@ function LoadExampleCode(filename, wd = nothing)
   return (
       "layout" => html_div(
           className = "example-container",
-          style = ("margin-bottom" => "10px",)
+          children = layout,
+          style = Dict("margin-bottom" => "10px"),
       ),
       "source_code" => html_div(
           children = dcc_markdown(
-              @sprintf("```julia\n%s```", example_file_as_string)
+              @sprintf("```Julia\n%s\n```", example_file_as_string)
           ),
           className = "code-container",
-          style = ("border-left" => "thin lightgrey solid",)
+          style = Dict("border-left" => "thin lightgrey solid"),
       ),
   )
 end
@@ -36,14 +37,14 @@ function LoadAndDisplayComponent(example_string)
   return html_div(
     (
       html_div(
-        children = dcc_markdown(@printf("```julia %s```", example_string)),
+        children = dcc_markdown(@printf("```Julia\n%s```", example_string)),
         className = "code-container",
-        style = ("border-left" => "thin lightgrey solid",)
+        style = Dict("border-left" => "thin lightgrey solid")
       ),
       html_div(
-        children = include_string(Main, example_string)
+        children = include_string(Main, example_string),
         className = "example-container",
-        style = ("margin-bottom" => "10px", "overflow-x" => "initial", )
+        style = Dict("margin-bottom" => "10px", "overflow-x" => "initial")
       ),
     )
   )
@@ -53,14 +54,14 @@ function LoadAndDisplayComponent2(example_string)
   return html_div(
     (
       html_div(
-        children = dcc_markdown(@printf("```julia %s```", example_string)),
+        children = dcc_markdown(@printf("```Julia\n%s```", example_string)),
         className = "code-container",
-        style = ("border-left" => "thin lightgrey solid",)
+        style = Dict("border-left" => "thin lightgrey solid")
       ),
       html_div(
         children = include_string(Main, example_string),
         className = "example-container",
-        style = ("margin-bottom" => "10px", "padding-bottom" => "30px", )
+        style = Dict("margin-bottom" => "10px", "padding-bottom" => "30px")
       ),
     )
   )
